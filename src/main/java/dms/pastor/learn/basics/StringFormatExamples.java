d3889152d2325342c350464145531c0ac86b39a8
Dominik Symonowicz <dmspastor@gmail.com>
2017-09-09 10:18:54 +0100

    Add some rarely used by me string format that worth remember

---
+++ src/main/java/dms/pastor/learn/basics/StringFormatExamples.java
@@ -0,0 +1,47 @@
+package dms.pastor.learn.basics;
+
+/**
+ * Author Dominik Symonowicz
+ * Created 08/09/2017
+ * WWW:	https://dominiksymonowicz.com/welcome
+ * IT BLOG:	https://dominiksymonowicz.blogspot.co.uk
+ * Github:	https://github.com/pastorcmentarny
+ * Google Play:	https://play.google.com/store/apps/developer?id=Dominik+Symonowicz
+ * LinkedIn: https://www.linkedin.com/in/dominik-symonowicz
+ * <p>
+ * tag-string-format
+ */
+class StringFormatExamples {
+    private final int width;
+
+    StringFormatExamples(int width) {
+        this.width = width;
+    }
+
+
+    String displayNumberAllignedToRightWithWidth20(int number) {
+        return String.format("|%" + width + "d|", number);
+    }
+
+    public String displayNumberAllignedToLeftWithWidth20(int number) {
+        return String.format("|%-" + width + "d|", number);
+    }
+
+    public String displayNumberAllignedToRightWithWidth20AndPadWithZeros(int number) {
+        return String.format("|%0" + width + "d|", number); // prints: |00000000000000000093|
+    }
+
+    public String displayPlusForPositiveNumber(int number) {
+        return String.format("|%+" + width + "d|", number);
+    }
+
+    public String displayNumberWithAmericanSeparator(int number) {
+        return String.format("|%,d|", number);
+    }
+
+    //TODO improve name
+    public String displayTextOfLength(String text, int length) {
+        return String.format("|%." + length + "s|", text);
+
+    }
+}
---
+++ src/test/java/dms/pastor/learn/basics/StringFormatExamplesTest.java
@@ -0,0 +1,89 @@
+package dms.pastor.learn.basics;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Author Dominik Symonowicz
+ * WWW:	https://dominiksymonowicz.com/welcome
+ * IT BLOG:	https://dominiksymonowicz.blogspot.co.uk
+ * Github:	https://github.com/pastorcmentarny
+ * Google Play:	https://play.google.com/store/apps/developer?id=Dominik+Symonowicz
+ * LinkedIn: https://www.linkedin.com/in/dominik-symonowicz
+ * <p>
+ * WARNING! Edge case tests omited as this is just example
+ */
+public class StringFormatExamplesTest {
+    private StringFormatExamples stringFormatExamples = new StringFormatExamples(5);
+    private int number = 168;
+
+    @Test
+    public void shouldPrintTextAlignedToTheRightWithSpecificWidth() {
+        // when
+        final String result = stringFormatExamples.displayNumberAllignedToRightWithWidth20(number);
+
+        // then
+        assertThat(result).isEqualTo("|  " + 168 + "|");
+    }
+
+    @Test
+    public void shouldPrintTextAlignedToTheLeftWithSpecificWidth() {
+        // when
+        final String result = stringFormatExamples.displayNumberAllignedToLeftWithWidth20(number);
+
+        // then
+        assertThat(result).isEqualTo("|" + 168 + "  |");
+    }
+
+    @Test
+    public void shouldPrintTextAlignedToTheRightWithSpecificWidthAndPadWithZeros() {
+        // when
+        final String result = stringFormatExamples.displayNumberAllignedToRightWithWidth20AndPadWithZeros(number);
+
+        // then
+        assertThat(result).isEqualTo("|00" + 168 + "|");
+    }
+
+    @Test
+    public void shouldDisplayNumberWithPlusForPositiveNumber() {
+        // when
+        final String result = stringFormatExamples.displayPlusForPositiveNumber(number);
+
+        // then
+        assertThat(result).isEqualTo("| +" + number + "|");
+    }
+
+    @Test
+    public void shouldDisplayNumberWithoutPlusForNegativeNumber() {
+        // when
+        final String result = stringFormatExamples.displayPlusForPositiveNumber(-number);
+
+        // then
+        assertThat(result).isEqualTo("| -" + number + "|");
+    }
+
+    @Test
+    public void shouldDisplayNumberWithoCustomSeparator() {
+        // given
+        final int thousandOne = 1001;
+        // when
+        final String result = stringFormatExamples.displayNumberWithAmericanSeparator(thousandOne);
+
+        // then
+        assertThat(result).isEqualTo("|1,001|");
+    }
+
+    @Test
+    public void shouldDisplayFirstFiveCharactersOfText() {
+        // given
+        final String text = "Warm Welcome To YOU";
+        final int length = 4;
+
+        // when
+        final String result = stringFormatExamples.displayTextOfLength(text, length);
+
+        // then
+        assertThat(result).isEqualTo("|Warm|");
+    }
+}
