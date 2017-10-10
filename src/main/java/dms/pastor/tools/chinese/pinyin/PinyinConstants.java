fe3e196febd7b49f9371038ce4410484f6053677
Dominik Symonowicz <dmspastor@gmail.com>
2017-10-10 02:09:49 +0100

    Add Pinyin Constants

---
+++ src/main/java/dms/pastor/tools/chinese/pinyin/PinyinConstants.java
@@ -0,0 +1,42 @@
+package dms.pastor.tools.chinese.pinyin;
+
+/**
+ * Author Dominik Symonowicz
+ * Created 07/10/2017
+ * WWW:	https://dominiksymonowicz.com/welcome
+ * IT BLOG:	https://dominiksymonowicz.blogspot.co.uk
+ * Github:	https://github.com/pastorcmentarny
+ * Google Play:	https://play.google.com/store/apps/developer?id=Dominik+Symonowicz
+ * LinkedIn: https://www.linkedin.com/in/dominik-symonowicz
+ */
+public class PinyinConstants {
+    public static final String A_NEUTRAL = "a";
+    public static final String A_FIRST_NOTE = "ā";
+    public static final String A_SECOND_TONE = "á";
+    public static final String A_THIRD_TONE = "ǎ";
+    public static final String A_FOURTH_TONE = "à";
+    public static final String E_NEUTRAL = "e";
+    public static final String E_FIRST_NOTE = "ē";
+    public static final String E_SECOND_TONE = "é";
+    public static final String E_THIRD_TONE = "ě";
+    public static final String E_FOURTH_TONE = "è";
+    public static final String O_NEUTRAL = "o";
+    public static final String O_FIRST_NOTE = "ō";
+    public static final String O_SECOND_TONE = "ó";
+    public static final String O_THIRD_TONE = "ǒ";
+    public static final String O_FOURTH_TONE = "ò";
+    public static final String I_NEUTRAL = "i";
+    public static final String I_FIRST_NOTE = "ī";
+    public static final String I_SECOND_TONE = "í";
+    public static final String I_THIRD_TONE = "ǐ";
+    public static final String I_FOURTH_TONE = "ì";
+    public static final String U_NEUTRAL = "u";
+    public static final String U_FIRST_NOTE = "ū";
+    public static final String U_SECOND_TONE = "ú";
+    public static final String U_THIRD_TONE = "ǔ";
+    public static final String U_FOURTH_TONE = "ù";
+
+    private PinyinConstants() {
+    }
+
+}
