a94c54a4a54e9dc9cee1c68ce082bf2dd32b1996
Dominik Symonowicz <dmspastor@gmail.com>
2017-10-10 03:58:54 +0100

    export Word from Doms learn Chinese project to add some validations to check word in dictionary in DLC

---
+++ src/main/java/dms/pastor/tools/chinese/validator/Word.java
@@ -0,0 +1,173 @@
+package dms.pastor.tools.chinese.validator;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+/**
+ * Author Dominik Symonowicz
+ * Created 07/10/2017
+ * WWW:	https://dominiksymonowicz.com/welcome
+ * IT BLOG:	https://dominiksymonowicz.blogspot.co.uk
+ * Github:	https://github.com/pastorcmentarny
+ * Google Play:	https://play.google.com/store/apps/developer?id=Dominik+Symonowicz
+ * LinkedIn: https://www.linkedin.com/in/dominik-symonowicz
+ * <p>
+ * This tool is used to validate words while they are loaded to dictionary in Doms learn Chinese
+ * This is slightly modified version of Word class from my game Dom Learn Chinese
+ * Validator is here not in App due fact that testing in Android is pain in the ass
+ */
+public final class Word {
+
+    private static final String DEFAULT_NOTE = "none";
+    private int id;
+    private String chineseCharacter;
+    private String pinyin;
+    private int strokes;
+    private String englishMeaning;
+    private String polish;
+    private String[] groups;
+    private String notes = DEFAULT_NOTE;
+    private int difficulty;
+
+    public Word(int id, String chineseCharacter, String pinyin, int strokes, String englishMeaning, String polish, String[] groups, String notes, int difficulty) {
+        setId(id);
+        setChineseCharacter(chineseCharacter);
+        setPinyin(pinyin);
+        setStrokes(strokes);
+        setEnglishMeaning(englishMeaning);
+        setPolishMeaning(polish);
+        setGroups(groups);
+        setNotes(notes);
+        setDifficulty(difficulty);
+    }
+
+    public static Word noWord() {
+        return new Word(-1, null, null, -1, null, null, null, null, -1);
+    }
+
+    public String getChineseCharacter() {
+        return chineseCharacter;
+    }
+
+    private void setChineseCharacter(String chineseCharacter) {
+        this.chineseCharacter = chineseCharacter;
+    }
+
+    public int getDifficulty() {
+        return difficulty;
+    }
+
+    private void setDifficulty(int difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public String getPinyin() {
+        return pinyin;
+    }
+
+    private void setPinyin(String pinyin) {
+        this.pinyin = pinyin;
+    }
+
+    public String getWordInEnglish() {
+        return englishMeaning;
+    }
+
+    private void setEnglishMeaning(String wEnglish) {
+        this.englishMeaning = wEnglish;
+    }
+
+    public String getWordInPolish() {
+        return polish;
+    }
+
+    private void setPolishMeaning(String wPolish) {
+        this.polish = wPolish;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    private void setId(int id) {
+        this.id = id;
+    }
+
+    public int getStrokes() {
+        return strokes;
+    }
+
+    private void setStrokes(int strokes) {
+        this.strokes = strokes;
+    }
+
+    public String[] getGroups() {
+        return groups;
+    }
+
+    private void setGroups(String[] groups) {
+        this.groups = groups;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    private void setNotes(String note) {
+        if (note == null) {
+            this.notes = DEFAULT_NOTE;
+        } else {
+            this.notes = note;
+        }
+
+    }
+
+    public String toShortString() {
+        return chineseCharacter + " - '" +
+                pinyin + "' - [ " +
+                englishMeaning + " ]";
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Word)) return false;
+        Word word = (Word) o;
+        return getId() == word.getId() &&
+                getStrokes() == word.getStrokes() &&
+                getDifficulty() == word.getDifficulty() &&
+                Objects.equals(getChineseCharacter(), word.getChineseCharacter()) &&
+                Objects.equals(getPinyin(), word.getPinyin()) &&
+                Objects.equals(englishMeaning, word.englishMeaning) &&
+                Objects.equals(polish, word.polish) &&
+                Arrays.equals(getGroups(), word.getGroups()) &&
+                Objects.equals(getNotes(), word.getNotes());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getChineseCharacter(), getPinyin(), getStrokes(), englishMeaning, polish, getGroups(), getNotes(), getDifficulty());
+    }
+
+    @Override
+    public String toString() {
+        return "Word{" +
+                "id=" + id +
+                ", chineseCharacter='" + chineseCharacter + '\'' +
+                ", pinyin='" + pinyin + '\'' +
+                ", strokes=" + strokes +
+                ", englishMeaning='" + englishMeaning + '\'' +
+                ", polish='" + polish + '\'' +
+                ", groups=" + Arrays.toString(groups) +
+                ", notes='" + notes + '\'' +
+                ", difficulty=" + difficulty +
+                '}';
+    }
+
+
+    public String asWord() {
+        return "ID: " + id + "\nChinese: " + chineseCharacter + "\nPinyin: " + pinyin + "(" + strokes + ")\nEnglish: " + englishMeaning + "\nPolish:" + polish + "\nNotes: " + notes + "\nDifficulty: " + difficulty;
+    }
+
+}
---
+++ src/test/java/dms/pastor/tools/chinese/validator/WordTest.java
@@ -0,0 +1,90 @@
+package dms.pastor.tools.chinese.validator;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Author Dominik Symonowicz
+ * Created 07/10/2017
+ * WWW:	https://dominiksymonowicz.com/welcome
+ * IT BLOG:	https://dominiksymonowicz.blogspot.co.uk
+ * Github:	https://github.com/pastorcmentarny
+ * Google Play:	https://play.google.com/store/apps/developer?id=Dominik+Symonowicz
+ * LinkedIn: https://www.linkedin.com/in/dominik-symonowicz
+ */
+public class WordTest {
+    private static final int DEFAULT_ID = 292;
+    private static final String DEFAULT_CHINESE_CHARACTER = "字";
+    private static final String DEFAULT_PINYIN = "zì";
+    private static final int DEFAULT_STROKES = 6;
+    private static final String DEFAULT_MEANING_IN_ENGLISH = "character,validWord";
+    private static final String DEFAULT_MEANING_IN_POLISH = "znak";
+    private static final String[] DEFAULT_GROUP = {"hsk1"};
+    private static final String DEFAULT_NOTES = "note";
+    private static final int DEFAULT_DIFFICULTY = 2;
+
+    private final Word word = new Word(DEFAULT_ID, DEFAULT_CHINESE_CHARACTER, DEFAULT_PINYIN, DEFAULT_STROKES,
+            DEFAULT_MEANING_IN_ENGLISH, DEFAULT_MEANING_IN_POLISH, DEFAULT_GROUP, DEFAULT_NOTES, DEFAULT_DIFFICULTY);
+
+    @Test
+    public void noWordShouldCreateNoWordObject() {
+        // given
+        final Word expectedWord = new Word(-1, null, null, -1,
+                null, null, null, null, -1);
+
+        // when
+        final Word word = Word.noWord();
+
+        // then
+        assertThat(word).isEqualTo(expectedWord);
+    }
+
+    @Test
+    public void toShortStringShouldrRetunBasicInformationAboutWord() {
+        // given
+        final String expectedWordWithBasicInformation = word.getChineseCharacter() + " - '" +
+                word.getPinyin() + "' - [ " +
+                word.getWordInEnglish() + " ]";
+
+        // when
+        final String wordWithBasicInformation = word.toShortString();
+
+        // then
+        assertThat(wordWithBasicInformation).isEqualTo(expectedWordWithBasicInformation);
+    }
+
+    @Test
+    public void asWordShouldRetunWordAsString() {
+        // given
+        final String expectedWordAsStringWithAllInformation = "ID: " + word.getId()
+                + "\nChinese: " + word.getChineseCharacter() + "\nPinyin: " + word.getPinyin() + "(" + word.getStrokes()
+                + ")\nEnglish: " + word.getWordInEnglish() + "\nPolish:" + word.getWordInPolish()
+                + "\nNotes: " + word.getNotes() + "\nDifficulty: " + word.getDifficulty();
+
+        // when
+        final String wordAsString = word.asWord();
+
+        // then
+        assertThat(wordAsString).isEqualTo(expectedWordAsStringWithAllInformation);
+    }
+
+
+    @Test
+    public void setNotesShouldSetNoteToNoneIfNoteIsNull() {
+        // given
+        final String nullNote = null;
+        final String expectedNote = "none";
+
+        final Word wordWithNullNote = new Word(DEFAULT_ID, DEFAULT_CHINESE_CHARACTER, DEFAULT_PINYIN,
+                DEFAULT_STROKES, DEFAULT_MEANING_IN_ENGLISH, DEFAULT_MEANING_IN_POLISH,
+                DEFAULT_GROUP, nullNote, DEFAULT_DIFFICULTY);
+
+        // when
+        final String notes = wordWithNullNote.getNotes();
+
+        // then
+        assertThat(notes).isEqualTo(expectedNote);
+    }
+
+}
