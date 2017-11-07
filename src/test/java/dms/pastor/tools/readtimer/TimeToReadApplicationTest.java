8518fe36ddef921dc3f2cb6819e7ac6955be0377
Dominik Symonowicz <dmspastor@gmail.com>
2017-11-07 13:28:43 +0000

    add missing tests for Time to read application

--- src/test/java/dms/pastor/tools/readtimer/TimeToReadApplicationTest.java
+++ src/test/java/dms/pastor/tools/readtimer/TimeToReadApplicationTest.java
@@ -1,9 +1,26 @@
 package dms.pastor.tools.readtimer;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.Appender;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 import static dms.pastor.utils.randoms.RandomDataGenerator.generateString;
 import static dms.pastor.utils.randoms.RandomDataGenerator.randomPositiveInteger;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
 
 /**
  * Author Dominik Symonowicz
@@ -14,9 +31,28 @@ import static dms.pastor.utils.randoms.RandomDataGenerator.randomPositiveInteger
  * Google Play:	https://play.google.com/store/apps/developer?id=Dominik+Symonowicz
  * LinkedIn: https://www.linkedin.com/in/dominik-symonowicz
  */
+@RunWith(MockitoJUnitRunner.class)
 public class TimeToReadApplicationTest {
 
-    //TODO
+    @SuppressWarnings("StaticFieldReferencedViaSubclass") // used for testing
+    private static final Logger LOGGER = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+
+    @Mock
+    private Appender<ILoggingEvent> mockAppender;
+
+    @Captor
+    private ArgumentCaptor<ILoggingEvent> captorLoggingEvent;
+
+    @Before
+    public void setUp() throws Exception {
+        LOGGER.addAppender(mockAppender);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        LOGGER.detachAppender(mockAppender);
+    }
+
     @Test
     public void shouldRunDemoWhenRunningWithoutArgumentsAcceptanceTest() throws Exception {
         // given
@@ -25,7 +61,16 @@ public class TimeToReadApplicationTest {
         // when
         TimeToReadApplication.main(args);
 
-        // then check for is result appear in logs
+        // then assert logs
+        verify(mockAppender, atLeastOnce()).doAppend(captorLoggingEvent.capture());
+        final List<ILoggingEvent> loggingEvents = captorLoggingEvent.getAllValues();
+        assertThat(loggingEvents.get(0).getLevel()).isEqualTo(Level.INFO);
+        assertThat(loggingEvents.get(1).getLevel()).isEqualTo(Level.INFO);
+        assertThat(loggingEvents.get(2).getLevel()).isEqualTo(Level.DEBUG);
+        assertThat(loggingEvents.get(3).getLevel()).isEqualTo(Level.INFO);
+        assertThat(loggingEvents.get(0).getFormattedMessage()).contains("Starting application..");
+        assertThat(loggingEvents.get(2).getFormattedMessage()).contains("Words:1");
+        assertThat(loggingEvents.get(3).getFormattedMessage()).contains("RIP .. at ");
     }
 
 }
