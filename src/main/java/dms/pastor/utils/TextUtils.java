94c8efcddd93ce4be75d4718a03f3b983186a289
Dominik Symonowicz <dmspastor@gmail.com>
2017-12-29 09:20:51 +0000

    (as part of playing with regex and scanner and as future for my android app) Add count word in text add get list of paragraphs

---
+++ src/main/java/dms/pastor/utils/TextUtils.java
@@ -0,0 +1,46 @@
+package dms.pastor.utils;
+
+import dms.pastor.domain.exception.SomethingWentWrongException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class TextUtils {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TextUtils.class);
+    private static final String ERROR_MESSAGE = "Unable to count word due ";
+
+    public static long countWordInTextInFile(String fileName) {
+        try (Scanner scanner = new Scanner(new File(fileName))) {
+            long counter = 0;
+            while (scanner.hasNext()) {
+                scanner.next();
+                counter++;
+            }
+            return counter;
+        } catch (Exception exception) {
+            final String errorMessage = ERROR_MESSAGE + exception.getMessage();
+            LOGGER.error(errorMessage, exception);
+            throw new SomethingWentWrongException(errorMessage, exception);
+        }
+    }
+
+    public static List<String> getTextInParagraphs(String fileName) {
+        try (Scanner scanner = new Scanner(new File(fileName))) {
+            scanner.useDelimiter("(?m:^$)");
+            List<String> paragraphs = new ArrayList<>();
+            while (scanner.hasNext()) {
+                String paragraph = scanner.next();
+                paragraphs.add(paragraph);
+            }
+            return paragraphs;
+        } catch (Exception exception) {
+            final String errorMessage = ERROR_MESSAGE + exception.getMessage();
+            LOGGER.error(errorMessage, exception);
+            throw new SomethingWentWrongException(errorMessage, exception);
+        }
+    }
+}
---
+++ src/test/java/dms/pastor/utils/TextUtilsTest.java
@@ -0,0 +1,60 @@
+package dms.pastor.utils;
+
+import dms.pastor.domain.exception.SomethingWentWrongException;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.util.List;
+
+import static dms.pastor.TestConfig.PATH;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TextUtilsTest {
+    private static final String FILE_PATH = PATH + "test.txt";
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void countWordInTextInFileShouldThrowSomethingWentWrongExceptionIfAnyExceptionIsThrownDuingCounting() {
+        // expect
+        exception.expect(SomethingWentWrongException.class);
+        exception.expectMessage("Unable to count word due null");
+
+        // when
+        TextUtils.countWordInTextInFile(null);
+    }
+
+    @Test
+    public void countWordInTextInFileShouldReturn8Words() {
+        // when
+        final long result = TextUtils.countWordInTextInFile(FILE_PATH);
+
+        // then
+        assertThat(result).isEqualTo(8);
+    }
+
+    @Test
+    public void getTextInParagrapsShouldThrowSomethingWentWrongExceptionIfAnyExceptionIsThrownDuingCounting() {
+        // expect
+        exception.expect(SomethingWentWrongException.class);
+        exception.expectMessage("Unable to count word due null");
+
+        // when
+        TextUtils.getTextInParagraphs(null);
+    }
+
+    @Test
+    public void getTextInParagrapsShouldReturnArrayOfParagraphs() {
+        // given
+        final String expectedParagraph = "This is a default test file" + System.lineSeparator() + "test" + System.lineSeparator();
+
+        // when
+        final List<String> result = TextUtils.getTextInParagraphs(FILE_PATH);
+
+        // then
+        assertThat(result).hasSize(2);
+        assertThat(result.get(0)).isEqualTo(expectedParagraph);
+    }
+}
