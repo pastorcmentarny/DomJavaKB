98030e869bb97e88e0e3b53382217c11a2993e35
Dominik Symonowicz <dmspastor@gmail.com>
2018-01-15 09:23:18 +0000

    Add DrawListExtractor prototype

---
+++ src/main/java/dms/pastor/tools/lotto/accuracy/check/DrawListExtractor.java
@@ -0,0 +1,31 @@
+package dms.pastor.tools.lotto.accuracy.check;
+
+import dms.pastor.tools.lotto.hotpick.HotPickDraw;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static dms.pastor.utils.ValidatorUtils.*;
+
+public final class DrawListExtractor {
+
+    public static List<HotPickDraw> getHotPickDrawListFromRange(List<HotPickDraw> drawList, int startRange, int endRange) {
+        validateInput(drawList, startRange, endRange);
+        List<HotPickDraw> draws = new ArrayList<>(endRange - startRange);
+
+        for (int i = startRange; i <= endRange; i++) {
+            draws.add(drawList.get(i - 1));
+        }
+        return draws;
+    }
+
+    private static void validateInput(List<HotPickDraw> drawList, int startRange, int endRange) {
+        validateIfNotNull(drawList, "Hot Picks Draw");
+        validateIfNotNull(startRange, "Start Range");
+        validateIfNotNull(endRange, "End Range");
+        validateMinValueIsSmallerThanMaxValue(startRange, endRange);
+        validateMinValueIsSmallerThanMaxValue(startRange, endRange);
+        validateValueIsSmallerOrEqualsThatOtherValue(startRange, drawList.size());
+        validateValueIsSmallerOrEqualsThatOtherValue(endRange, drawList.size());
+    }
+}
