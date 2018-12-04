25b2fbf14cec1e9bbf9fff7e59f3b129ba11c537
Dominik Symonowicz <dmspastor@gmail.com>
2018-12-04 21:15:24 +0000

    add my default bash script for mac

---
+++ bash_scripts/bash.sh
@@ -0,0 +1,3 @@
+alias j11="export JAVA_HOME=/usr/libexec/java_home -v 11; java -version"
+alias j10="export JAVA_HOME=/usr/libexec/java_home -v 10; java -version"
+alias j8="export JAVA_HOME=/usr/libexec/java_home -v 1.8; java -version"
