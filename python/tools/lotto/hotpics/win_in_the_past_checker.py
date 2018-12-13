fce97a7120b4a09412b86f4b03016fa31f1f4249
Dominik Symonowicz <dmspastor@gmail.com>
2018-12-13 13:58:13 +0000

    code refactoring ..

--- python/tools/lotto/hotpics/win_in_the_past_checker.py
+++ python/tools/lotto/hotpics/win_in_the_past_checker.py
@@ -1,6 +1,6 @@
 import csv
 import logging
-import os
+import sys
 
 # this app is used to provide information for given numbers how many times you won. it will check for 3,4,5,5+ and 6 number
 # validate numbers to ensure is 6 of them and they are unique
@@ -11,36 +11,27 @@ import os
 base_dir = '../../../../data/lotto/'
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s',
                     filename=base_dir + 'log.txt')
+sys.path.insert(0, '../utils')
+import past_result_checker
+
 
 # numbers = ['2 ', '5 ', '14', '22', '44']
-numbers = ['3 ', '20', '29', '32', '35', '43']
+numbers = ['51', '46', '59', '31', '22']
 
 
 def main():
-    logging.debug("checking if numbers you selected won in the past results")
-    info()
-    validate()
+    logging.debug("checking if numbers you selected won in the past results for LOTTO HOTPICS")
 
     lotto_hotpicks_history_path = base_dir + 'lotto-hotpicks-all-draws.csv'
     lotto_hotpics_draw_history_file = open(lotto_hotpicks_history_path)
+
+    print("If no result, ensure you format all results in " + lotto_hotpicks_history_path)
 
     # load data
     euro_hotpics_history_csv = csv.reader(lotto_hotpics_draw_history_file)
     data = list(euro_hotpics_history_csv)
 
-    print("6's : " + str(count_hits(data, 6)))
-    print("5's : " + str(count_hits(data, 5)))
-    print("4's : " + str(count_hits(data, 4)))
-    print("3's : " + str(count_hits(data, 3)))
-    print("2's : " + str(count_hits(data, 2)))
-    print("1's : " + str(count_hits(data, 1)))
-
-    print("If no result, ensure you format all results in " + lotto_hotpicks_history_path)
-
-
-def info():
-    print('Numbers' + str(numbers))
-    print(os.getcwd())
+    past_result_checker.check_for(True,numbers, data)
 
 
 def validate():
@@ -48,26 +39,6 @@ def validate():
         raise ValueError("Numbers are not equal")
     else:
         print("Validation done. Given numbers are unique.")
-
-
-# improve it
-def count_hits(data: list, must_hit: int):
-    counter = 0
-    for draw in data:
-        hit = 0
-        if numbers[0] in draw:
-            hit += 1
-        if numbers[1] in draw:
-            hit += 1
-        if numbers[2] in draw:
-            hit += 1
-        if numbers[3] in draw:
-            hit += 1
-        if numbers[4] in draw:
-            hit += 1
-        if hit >= must_hit:
-            counter += 1
-    return counter
 
 
 if __name__ == '__main__':
