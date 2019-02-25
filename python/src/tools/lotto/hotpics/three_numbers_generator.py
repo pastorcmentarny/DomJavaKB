9eed4ff3a8e5b5b4f909a57dc698b9ee97a4cf6f
Dominik Symonowicz <dmspastor@gmail.com>
2019-02-25 09:18:52 +0000

    design

---
+++ python/src/tools/lotto/hotpics/three_numbers_generator.py
@@ -0,0 +1,5 @@
+# collect all possible triple combination from all games
+# find out which numbers didn't play in last 10 draws
+# pickup triple that haven't been played yet,
+# discard most and least drawn number pick .. 2 second most/least played number and 1 random number from pool  has max number bigger than 49
+# display all result
