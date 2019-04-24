f463e10ba2596124479f5e748b508a4985f5cb51
Dominik Symonowicz <dmspastor@gmail.com>
2019-04-24 11:25:06 +0100

    improve test coverage

---
+++ java/src/test/java/dms/pastor/rpg/places/mine/aberminegenerator/RPGWorldGeneratorTest.java
@@ -0,0 +1,19 @@
+package dms.pastor.rpg.places.mine.aberminegenerator;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RPGWorldGeneratorTest {
+
+    @Test
+    public void shouldRunGenerator() {
+        // given
+
+        // when
+        Generator.main(null);
+
+        // then
+
+    }
+}
