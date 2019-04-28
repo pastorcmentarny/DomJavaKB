f389913bec0461f4eab3fe3b283cb097b1843a9a
Dominik Symonowicz <dmspastor@gmail.com>
2019-04-28 19:38:18 +0100

    add new interesting methods that are added in Java 12 that I may use once in my lifetime

---
+++ java/src/main/java/dms/pastor/examples/java12/NewFeaturesInJava12.java
@@ -0,0 +1,43 @@
+package dms.pastor.examples.java12;
+
+import java.text.NumberFormat;
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * Author Dominik Symonowicz
+ * Created 28/04/2019
+ * WWW:	http://pastor.ovh.org
+ * Github:	https://github.com/pastorcmentarny
+ * Google Play:	https://play.google.com/store/apps/developer?id=Dominik+Symonowicz
+ * LinkedIn: uk.linkedin.com/pub/dominik-symonowicz/5a/706/981/
+ */
+public class NewFeaturesInJava12 {
+
+    private NewFeaturesInJava12(){}
+
+    public static String returnsClassForAnArrayType() {
+        return "dominik".getClass().arrayType().toString();
+    }
+
+    public static String stringIntentMethod() {
+        return "Dominik".indent(3);
+
+    }
+
+    public static String compactNumberFormat() {
+        final Locale locale = Locale.UK;
+        List<Integer> numbers = List.of(1000, 1000000);
+        final var stringBuilder = new StringBuilder();
+        numbers.forEach((integer) -> {
+            NumberFormat shortNumberFormat = NumberFormat.getCompactNumberInstance(locale, NumberFormat.Style.SHORT);
+            NumberFormat longNumberFormat = NumberFormat.getCompactNumberInstance(locale, NumberFormat.Style.LONG);
+            String formattedShortNumber = shortNumberFormat.format(integer);
+            String formattedLongNumber = longNumberFormat.format(integer);
+            stringBuilder.append("short: ").append(formattedShortNumber).append(" long: ").append(formattedLongNumber).append("\n");
+        });
+        return stringBuilder.toString();
+    }
+
+    //TODO return "Dominik".transform(null);
+}
---
+++ java/src/test/java/dms/pastor/examples/java12/NewFeaturesInJava12Test.java
@@ -0,0 +1,26 @@
+package dms.pastor.examples.java12;
+
+import dms.pastor.ExampleRunner;
+import org.junit.Test;
+
+import static dms.pastor.examples.java12.NewFeaturesInJava12.*;
+
+/**
+ * Author Dominik Symonowicz
+ * Created 28/04/2019
+ * WWW:	http://pastor.ovh.org
+ * Github:	https://github.com/pastorcmentarny
+ * Google Play:	https://play.google.com/store/apps/developer?id=Dominik+Symonowicz
+ * LinkedIn: uk.linkedin.com/pub/dominik-symonowicz/5a/706/981/
+ */
+public class NewFeaturesInJava12Test implements ExampleRunner {
+
+    @Test
+    @Override
+    public void runExamples()  {
+        System.out.println(returnsClassForAnArrayType());
+        System.out.println(stringIntentMethod());
+        System.out.println(compactNumberFormat());
+
+    }
+}
