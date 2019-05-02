d1bee66f3dbbd5c7659141167b8774e0e7daa413
Dominik Symonowicz <dmspastor@gmail.com>
2019-05-02 10:23:03 +0100

    create skeleton app for revenger

---
+++ revenger/src/main/java/dms/pastor/revenger/RevengerApplication.java
@@ -0,0 +1,13 @@
+package dms.pastor.revenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevengerApplication.class, args);
+	}
+
+}
---
+++ revenger/src/test/java/dms/pastor/revenger/RevengerApplicationTest.java
@@ -0,0 +1,16 @@
+package dms.pastor.revenger;
+
+import org.junit.Test;
+
+public class RevengerApplicationTest {
+
+    @Test
+    public void shouldRunApplication() {
+        // given
+
+        // when
+        RevengerApplication.main(new String[0]);
+        // then
+
+    }
+}
