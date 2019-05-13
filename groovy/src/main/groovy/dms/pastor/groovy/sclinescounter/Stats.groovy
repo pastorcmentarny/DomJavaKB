686222d7878f79056a9903f1fcf2a6ef6cad22c5
Dominik Symonowicz <dmspastor@gmail.com>
2019-05-13 11:10:46 +0100

    code cleanup

--- groovy/src/main/groovy/dms/pastor/groovy/sclinescounter/Stats.groovy
+++ groovy/src/main/groovy/dms/pastor/groovy/sclinescounter/Stats.groovy
@@ -9,49 +9,48 @@ class Stats {
     def static PATH_TO_DIRECTORY = ["C:\\ds\\projects\\DomJavaKB"]
 
     static main(args) {
-		println("Stats for Dom Linker")
+        println("Stats for Dom Linker")
         //set date and time stuff
-        def start = new GregorianCalendar(2013,10,28)
+        def start = new GregorianCalendar(2013, 10, 28)
         def calendarInstance = Calendar.getInstance()
-        def now = new GregorianCalendar(calendarInstance.get(Calendar.YEAR),calendarInstance.get(Calendar.MONTH),calendarInstance.get(Calendar.DAY_OF_MONTH))
-        
+        def now = new GregorianCalendar(calendarInstance.get(Calendar.YEAR), calendarInstance.get(Calendar.MONTH), calendarInstance.get(Calendar.DAY_OF_MONTH))
+
         long startAsLong = start.getTime().getTime()
         long nowAsLong = now.getTime().getTime() + 10000
-        def difMs = nowAsLong-startAsLong
-        def msPerDay = 1000*60*60*24
-        def daysSinceStart = (difMs/msPerDay).intValue()
-        
-        
+        def difMs = nowAsLong - startAsLong
+        def msPerDay = 1000 * 60 * 60 * 24
+        def daysSinceStart = (difMs / msPerDay).intValue()
+
+
         def dateFormat = 'dd-MM-yyyy' // set format (you can do this when you parse as well)
 
         //set target
         def target = 2500
 
         def amPM = Calendar.getInstance().get(Calendar.AM_PM)
-        if (amPM == Calendar.AM)
-        {
+        if (amPM == Calendar.AM) {
             println("Good morning")
         } else {
             println("Good day,afternoon or evening")
         }
-        println("Today is " +new Date().format(dateFormat))
+        println("Today is " + new Date().format(dateFormat))
 
         def num = 0
-		def files = 0
+        def files = 0
 
         def list = []
 
-		for (path in PATH_TO_DIRECTORY){
-			def dir = new File(path)
-			dir.eachFileRecurse (FileType.FILES) { file -> list << file }
-	
-			list.each {
-				num += countFor(it.path)
-			}
-			
-			files += list.size()
-	
-		}
+        for (path in PATH_TO_DIRECTORY) {
+            def dir = new File(path)
+            dir.eachFileRecurse(FileType.FILES) { file -> list << file }
+
+            list.each {
+                num += countFor(it.path)
+            }
+
+            files += list.size()
+
+        }
 
         //display result
         displayResult(num, list, target, daysSinceStart)
@@ -64,9 +63,9 @@ class Stats {
         println getHistoryOfProgress()
     }
 
-    static int countFor(String filePath){
+    static int countFor(String filePath) {
         def counter = 0
-        try{
+        try {
             new File(filePath).eachLine { counter++ }
         } catch (Exception ignored) {
             println("You mess something with path: " + filePath)
@@ -74,11 +73,11 @@ class Stats {
 
         return counter
     }
-	
-	static String getHistoryOfProgress(){
-		def history = new StringBuilder("\nHistory of progress:\n")
+
+    static String getHistoryOfProgress() {
+        def history = new StringBuilder("\nHistory of progress:\n")
         history.append($/04-11-2013 ' 75 lines '\n/$)
         history.append($/04-11-2013 ' 206589 lines  in 4585 files'\n/$)
-		history
-	}
+        history
+    }
 }
