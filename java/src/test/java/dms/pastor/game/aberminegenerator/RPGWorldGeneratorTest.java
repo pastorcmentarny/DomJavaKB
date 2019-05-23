80a658b7e7559e221dbe8020dfe957b22eef2105
Dominik Symonowicz <dmspastor@gmail.com>
2019-05-23 13:56:38 +0100

    uningore tests as their not failing

--- java/src/test/java/dms/pastor/game/aberminegenerator/RPGWorldGeneratorTest.java
+++ java/src/test/java/dms/pastor/game/aberminegenerator/RPGWorldGeneratorTest.java
@@ -1,19 +1,15 @@
 package dms.pastor.game.aberminegenerator;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class RPGWorldGeneratorTest {
 
-    @Ignore
     @Test
     public void shouldRunGenerator() {
-        // given
-
         // when
         Generator.main(null);
 
-        // then
+        // then no exception happen.
 
     }
 }
--- java/src/test/java/dms/pastor/game/aberminegenerator/model/PixelTest.java
+++ java/src/test/java/dms/pastor/game/aberminegenerator/model/PixelTest.java
@@ -1,16 +1,13 @@
 package dms.pastor.game.aberminegenerator.model;
 
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PixelTest {
 
-
-    @Ignore
-    @Test //TODO improve it
+    @Test
     public void wallPixelAcceptanceTest() {
         // given
         Pixel pixel = Pixel.getWallPixel(10, 10);
