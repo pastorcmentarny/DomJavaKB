a618b63b524fd0bc7fc65f7536b9139bfdb11a7c
Dominik Symonowicz <dmspastor@gmail.com>
2019-06-14 11:35:38 +0100

    add uk mobile number validator

---
+++ java/src/main/java/dms/pastor/utils/vaildators/UKMobileNumberValidator.java
@@ -0,0 +1,46 @@
+package dms.pastor.utils.vaildators;
+
+import dms.pastor.utils.RegexUtils;
+import dms.pastor.utils.StringUtils;
+
+import java.util.Set;
+
+/**
+ * Information based on
+ * https://en.wikipedia.org/wiki/Telephone_numbers_in_the_United_Kingdom
+ */
+public class UKMobileNumberValidator {
+
+    private UKMobileNumberValidator() {
+    }
+
+    private static final Set<String> UK_NUMBER_PATTERN = Set.of("071", "072", "073", "074", "075", "07624", "077", "078", "079",
+        "+4471", " +4472", " +4473", " +4474", " +4475", " +447624, +4477", " +4478", "+4479",
+        "71", " 72", " 73", " 74", " 75", " 7624, 77", " 78", "79",
+        "4471", " 4472", " 4473", " 4474", " 4475", " 447624, 4477", " 4478", "4479",
+        "0071", " 0072", " 0073", " 0074", " 0075", " 007624, 0077", " 0078", "0079");
+
+    private static boolean startWithMobilePhonePattern(String phoneNumber) {
+        return UK_NUMBER_PATTERN.stream()
+            .anyMatch(phoneNumber::startsWith);
+    }
+
+    public static boolean validate(String number) {
+        if (StringUtils.isStringEmpty(number)) {
+            return false;
+        }
+
+        number = number.replaceAll(" ", "").trim();
+
+        if (hasNotExpectedLength(number) || !RegexUtils.hasOnlyNumbersAndPlusAndMinusSign(number)) {
+            return false;
+        }
+
+        return startWithMobilePhonePattern(number);
+    }
+
+
+    private static boolean hasNotExpectedLength(String number) {
+        return !(number.length() == 10 || number.length() == 11 || number.length() == 13);
+    }
+}
---
+++ java/src/test/java/dms/pastor/utils/vaildators/UKMobileNumberValidatorTest.java
@@ -0,0 +1,51 @@
+package dms.pastor.utils.vaildators;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static dms.pastor.utils.StringUtils.EMPTY_STRING;
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(Parameterized.class)
+public class UKMobileNumberValidatorTest {
+
+    private Boolean expectedResult;
+    private String number;
+
+    public UKMobileNumberValidatorTest(boolean expectedResult, String number) {
+        this.expectedResult = expectedResult;
+        this.number = number;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+            {FALSE, null}, {FALSE, EMPTY_STRING},
+            {FALSE, "+4871351123456"}, {FALSE, "-447912345678"},
+            {FALSE, "+4871351123456"}, {FALSE, "-447912345678"},
+            {TRUE, "+447912345678"}, {TRUE, "07912345678"}, {TRUE, "7912345678"},{TRUE, "0791 2345678"},
+            {FALSE, "+4479"}, {FALSE, "07"}, {FALSE, "791"},{FALSE, "0791 2345  8"},
+            {FALSE, "01545 570881"}, {FALSE, "02012345678"}, {FALSE, "03450774224"},
+            {FALSE, "0800 0123456"}
+        });
+    }
+
+    @Test
+    public void shouldVerifyIsNumberIsValidUKPhoneNumber() {
+        System.out.println("Verify is number: " + number + " is valid") ;
+
+        // when
+        final var result = UKMobileNumberValidator.validate(number);
+
+        // then
+        assertThat(result).isEqualTo(expectedResult);
+
+    }
+}
