7c326ae52ab025eb468e70d5c2d299e603548e59
Dominik Symonowicz <dmspastor@gmail.com>
2019-07-02 21:03:52 +0100

    add exception example

---
+++ python/src/kb/exceptions.py
@@ -0,0 +1,4 @@
+# define Python user-defined exceptions
+class GobshiteException(Exception):
+    print('Shit happens because you have no f..king clue what are you doing! Learn things and fix your mess!')
+    pass
