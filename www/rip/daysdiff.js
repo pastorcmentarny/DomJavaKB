c674c08018425fe5c6acaa8a3ee0029d18ffc262
Dominik Symonowicz <dmspastor@gmail.com>
2020-01-07 15:34:26 +0000

    code cleanup

--- www/rip/daysdiff.js
+++ www/rip/daysdiff.js
@@ -4,17 +4,14 @@ let ghosts = 4;
 function setSouls() {
     document.write('<span id="souls">' + souls + '</span>');
 }
+
 function setGhosts() {
     document.write('<span id="ghosts">' + ghosts + '</span>');
 }
+
 function daysSinceUpdate() {
-    var date1 = new Date('01/07/2020');
-    var date2 = new Date(Date.now());
-    var diffDays = date2.getDate() - date1.getDate();
-    if (document.getElementById) {
-        document.write('<span id="dayCount">' + diffDays + '</span>');
-        storeText = document.getElementById("highlight")
-    } else {
-        document.write(text);
-    }
+    let date1 = new Date('01/07/2020');
+    let date2 = new Date(Date.now());
+    let diffDays = date2.getDate() - date1.getDate();
+    document.write('<span id="dayCount">' + diffDays + '</span>');
 }
