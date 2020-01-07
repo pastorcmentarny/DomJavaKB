573e6f724754afa3e99d8266fc37fb8a646f80a5
Dominik Symonowicz <dmspastor@gmail.com>
2020-01-07 15:32:53 +0000

    update souls gone project with config

--- www/rip/daysdiff.js
+++ www/rip/daysdiff.js
@@ -1,11 +1,18 @@
+let souls = 0;
+let ghosts = 4;
+
+function setSouls() {
+    document.write('<span id="souls">' + souls + '</span>');
+}
+function setGhosts() {
+    document.write('<span id="ghosts">' + ghosts + '</span>');
+}
 function daysSinceUpdate() {
-    var date1 = new Date('01/06/2020');
+    var date1 = new Date('01/07/2020');
     var date2 = new Date(Date.now());
     var diffDays = date2.getDate() - date1.getDate();
-    xxx = diffDays;
-    console.log("It works");
     if (document.getElementById) {
-        document.write('<span id="dayCount">' + xxx + '</span>');
+        document.write('<span id="dayCount">' + diffDays + '</span>');
         storeText = document.getElementById("highlight")
     } else {
         document.write(text);
