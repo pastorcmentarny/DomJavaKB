e10a2b23d20ec431af517a9f43310673c5b27bac
Dominik Symonowicz <dmspastor@gmail.com>
2020-01-26 10:52:25 +0000

    add is open 24 hours method to OpenCloseTime

--- java/src/main/java/dms/pastor/tasks/pickup/OpenCloseTime.java
+++ java/src/main/java/dms/pastor/tasks/pickup/OpenCloseTime.java
@@ -1,11 +1,9 @@
 package dms.pastor.tasks.pickup;
 
-import dms.pastor.utils.ObjectUtils;
 import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalTime;
-import java.util.Objects;
 
 import static dms.pastor.utils.ObjectUtils.areAllObjectsNull;
 
@@ -17,5 +15,9 @@ public class OpenCloseTime {
 
     public boolean isClosedWholeDay() {
         return areAllObjectsNull(open, close);
+    }
+
+    public boolean isOpen24Hour() {
+        return open == LocalTime.MIDNIGHT && close == LocalTime.MIDNIGHT;
     }
 }
--- java/src/test/java/dms/pastor/tasks/pickup/OpenCloseTimeTest.java
+++ java/src/test/java/dms/pastor/tasks/pickup/OpenCloseTimeTest.java
@@ -4,6 +4,7 @@ package dms.pastor.tasks.pickup;
 import org.junit.Test;
 
 import java.time.LocalTime;
+import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -56,4 +57,42 @@ public class OpenCloseTimeTest {
         // then
         assertThat(isClosed).isFalse();
     }
+
+    @Test
+    public void shouldReturnTrueIfIfShopIsOpen24hrFromMidnightToMidnight() {
+        // given
+        final OpenCloseTime open24h = OpenCloseTime.builder()
+                .open(LocalTime.MIDNIGHT)
+                .close(LocalTime.MIDNIGHT)
+                .build();
+        // when
+        final boolean isOpen24h = open24h.isOpen24Hour();
+
+        // then
+        assertThat(isOpen24h).isTrue();
+    }
+
+    @Test
+    public void shouldReturnFalseIfIfShopIsNotOpen24hrIfOpenAndCloseTimeAreNotMidnight() {
+        // given
+        LocalTime open;
+        LocalTime close;
+        if (new Random().nextBoolean()) {
+            open = LocalTime.now();
+            close = LocalTime.MIDNIGHT;
+        } else {
+            open = LocalTime.MIDNIGHT;
+            close = LocalTime.now();
+        }
+        final OpenCloseTime isNotOpen24h = OpenCloseTime.builder()
+                .open(LocalTime.MIDNIGHT)
+                .close(LocalTime.now())
+                .build();
+        // when
+        final boolean isOpen24h = isNotOpen24h.isOpen24Hour();
+
+        // then
+        assertThat(isOpen24h).isFalse();
+    }
+
 }
