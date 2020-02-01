1638ab6efd25d206b4d767f0ebdd4dfb275e0ada
Dominik Symonowicz <dmspastor@gmail.com>
2020-02-01 08:02:03 +0000

    improve comment in increase_recursion_limit.py

--- python/src/tools/increase_recursion_limit.py
+++ python/src/tools/increase_recursion_limit.py
@@ -1,6 +1,7 @@
 import sys
 
-# example of increase recursion limit if you hit problem maximum recursion depth exceeded (Java StackOverflowError) in Jython
+# example of increase recursion limit
+# if you hit problem maximum recursion depth exceeded (Java StackOverflowError) in Jython
 
 limit = sys.getrecursionlimit()
 print('current recursion limit is:' + str(limit))
