8e92edcf736427d1b5666e40d5c6b000ce848b7d
Dominik Symonowicz <dmspastor@gmail.com>
2020-02-01 16:58:23 +0000

    update colourful text js that i used in my project

--- html/colourfulText.js
+++ html/colourfulText.js
@@ -1,61 +1,55 @@
-<
-script >
-var text = "Bądź pozdrowiony szanowny[lub nie] gościu" // YOUR TEXT
-var speed = 25 // SPEED OF FADE
-if (document.all || document.getElementById) {
-    document.write('<span id="highlight">' + text + '</span>')
-    var storetext = document.getElementById ? document.getElementById("highlight") : document.all.highlight
+const text = "Dom's The Unicorn Project";
+const speedOfFade = 64;
+const hex = ["00", "07", "14", "21", "28", "3C", "50", "64", "78", "8C", "A0", "B4", "C8", "DC", "F0"];
+let r, g, b, seq = 1;
+let storeText;
+if (document.getElementById) {
+    document.write('<h1 id="highlight">' + text + '</h1>');
+    storeText = document.getElementById("highlight")
 } else
-    document.write(text)
-var hex = new Array("00", "14", "28", "3C", "50", "64", "78", "8C", "A0", "B4", "C8", "DC", "F0")
-var r = 1
-var g = 1
-var b = 1
-var seq = 1
+    document.write(text);
 
-function changetext() {
-    rainbow = "#" + hex[r] + hex[g] + hex[b]
-    storetext.style.color = rainbow
+
+function changeText() {
+    storeText.style.color = `#${hex[r]}${hex[g]}${hex[b]}`
 }
 
 function change() {
-    if (seq == 6) {
-        b--
-        if (b == 0)
+    if (seq === 6) {
+        b--;
+        if (b === 0)
             seq = 1
     }
-    if (seq == 5) {
-        r++
-        if (r == 12)
+    if (seq === 5) {
+        r++;
+        if (r === 12)
             seq = 6
     }
-    if (seq == 4) {
-        g--
-        if (g == 0)
+    if (seq === 4) {
+        g--;
+        if (g === 0)
             seq = 5
     }
-    if (seq == 3) {
-        b++
-        if (b == 12)
+    if (seq === 3) {
+        b++;
+        if (b === 12)
             seq = 4
     }
-    if (seq == 2) {
-        r--
-        if (r == 0)
+    if (seq === 2) {
+        r--;
+        if (r === 0)
             seq = 3
     }
-    if (seq == 1) {
-        g++
-        if (g == 12)
+    if (seq === 1) {
+        g++;
+        if (g === 12)
             seq = 2
     }
-    changetext()
+    changeText()
 }
 
-function starteffect() {
-    if (document.all || document.getElementById)
-        flash = setInterval("change()", speed)
-}
 
-starteffect()
-< /script>
+function displayTitle() {
+    if (document.getElementById)
+        flash = setInterval("change()", speedOfFade)
+}
