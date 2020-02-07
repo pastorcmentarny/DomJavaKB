788ca8e2cc365bf2702589fe8c3abcd09cae71b2
Dominik Symonowicz <dmspastor@gmail.com>
2020-02-07 17:19:44 +0000

    clean RotateListExample class

--- java/src/main/java/dms/pastor/examples/RotateListExample.java
+++ java/src/main/java/dms/pastor/examples/RotateListExample.java
@@ -1,22 +1,22 @@
 package dms.pastor.examples;
 
-import dms.pastor.utils.PrintOutUtils;
-
-import java.time.*;
+import java.time.LocalDate;
 import java.time.format.TextStyle;
-import java.util.*;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
 
 import static dms.pastor.utils.DateUtils.getWeekDaysAsList;
 import static java.time.Month.JANUARY;
 
 public class RotateListExample {
 
-    public static String example(){
-        var wednesday = LocalDate.of(2020, JANUARY,22).getDayOfWeek().getDisplayName(TextStyle.FULL_STANDALONE, Locale.ENGLISH);
+    public static String example() {
+        var wednesday = LocalDate.of(2020, JANUARY, 22).getDayOfWeek().getDisplayName(TextStyle.FULL_STANDALONE, Locale.ENGLISH);
         List<String> allDays = getWeekDaysAsList();
         int index = allDays.indexOf(wednesday);
-        Collections.rotate(allDays,-index);
-        return String.format("Rotate by %d day(s). Start: %s Last day: %s",index,allDays.get(0), allDays.get(6));
+        Collections.rotate(allDays, -index);
+        return String.format("Rotate by %d day(s). Start: %s Last day: %s", index, allDays.get(0), allDays.get(6));
     }
 
 }
