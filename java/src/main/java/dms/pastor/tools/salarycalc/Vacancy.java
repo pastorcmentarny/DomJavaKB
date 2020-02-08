4589e005dd7147b17bb9a9fab1b42a630bab3035
Dominik Symonowicz <dmspastor@gmail.com>
2020-02-08 03:56:06 +0000

    create Vacancy with default values for ideal vacancy

---
+++ java/src/main/java/dms/pastor/tools/salarycalc/Vacancy.java
@@ -0,0 +1,34 @@
+package dms.pastor.tools.salarycalc;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Author Dominik Symonowicz
+ * Created 06/08/2018
+ * WWW:	https://dominiksymonowicz.com/welcome
+ * IT BLOG:	https://dominiksymonowicz.blogspot.co.uk
+ * Github:	https://github.com/pastorcmentarny
+ * Google Play:	https://play.google.com/store/apps/developer?id=Dominik+Symonowicz
+ * LinkedIn: https://www.linkedin.com/in/dominik-symonowicz
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class Vacancy {
+    private int salary = 60000;
+    private boolean inZone1 = false;
+    private boolean hatedStation = false;
+    private int annualLeaveDays = 27;
+    private int timeTravel = 21;
+    private boolean wfh = true;
+    private double hours = 35;
+
+
+    public static Vacancy getIdealVacancySalary() {
+        return new Vacancy();
+    }
+}
---
+++ java/src/test/java/dms/pastor/tools/salarycalc/VacancyTest.java
@@ -0,0 +1,21 @@
+package dms.pastor.tools.salarycalc;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class VacancyTest {
+
+    @Test
+    public void getIdealVacancySalaryAcceptanceTest() {
+        // given
+        Vacancy expectedIdealVacancy = new Vacancy();
+
+        // when
+        final var idealVacancySalary = Vacancy.getIdealVacancySalary();
+
+        // then
+        assertThat(idealVacancySalary).isEqualTo(expectedIdealVacancy);
+    }
+}
