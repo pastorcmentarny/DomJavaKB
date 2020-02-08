89467960abb193ad16262e29204ddd2d50ffee79
Dominik Symonowicz <dmspastor@gmail.com>
2020-02-08 12:57:52 +0000

    add prototype for SalaryApp

---
+++ java/src/main/java/dms/pastor/tools/salarycalc/SalaryApp.java
@@ -0,0 +1,44 @@
+package dms.pastor.tools.salarycalc;
+
+public class SalaryApp {
+    private Vacancy vacancy;
+
+    private SalaryApp() {
+        // set role details
+        vacancy = Vacancy.builder()
+                .salary(60000)
+                .hours(35)
+                .annualLeaveDays(24)
+                .timeTravel(53)
+                .hatedStation(true)
+                .inZone1(true)
+                .optionToBuyExtraDays(10)
+                .wfh(true)
+                .build();
+    }
+
+    public static void main(String[] args) {
+        final SalaryApp app = new SalaryApp();
+        app.generateReport();
+    }
+
+    private void generateReport() {
+        System.out.println(String.format("For this role I expect to earn: %d pounds.", new Calculator(vacancy).calculateSalary()));
+        System.out.println(getResultForRole());
+    }
+
+    private String getResultForRole() {
+        final int salary = new Calculator(vacancy).calculateSalary();
+        if (salary == vacancy.getSalary()) {
+            return "Perfect match!";
+        }
+        if (vacancy.getSalary() > salary) {
+            return String.format("Salary from vacancy is higher by %d british queens.", vacancy.getSalary() - salary);
+        }
+        if (salary - (salary / 13) > vacancy.getSalary()) {
+            return "Salary too low. Reject it!";
+        }
+        return "Salary is lower than my expectation but still worth to consider if tech stack and company are great.";
+    }
+
+}
