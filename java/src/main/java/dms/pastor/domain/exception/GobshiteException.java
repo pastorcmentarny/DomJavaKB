6a9c1f702e79705a1c04003ba16a44dd78d1fe06
Dominik Symonowicz <dmspastor@gmail.com>
2020-02-28 11:15:18 +0000

    remove unused exception

--- java/src/main/java/dms/pastor/domain/exception/GobshiteException.java
+++
@@ -1,7 +0,0 @@
-package dms.pastor.domain.exception;
-
-public class GobshiteException extends RuntimeException {
-    public GobshiteException(Throwable throwable) {
-        super("Shit happens because you have no f..king clue what are you doing! Learn things and fix your mess!", throwable);
-    }
-}
