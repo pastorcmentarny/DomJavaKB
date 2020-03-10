80e84e4d3730a014b41371aa627907258d2b8266
Dominik Symonowicz <dmspastor@gmail.com>
2020-03-10 09:51:39 +0000

    add toArrayOfTiles

--- rpg/src/main/java/dms/pastor/prototypes/aberminegenerator/model/PixelUtils.java
+++ rpg/src/main/java/dms/pastor/prototypes/aberminegenerator/model/PixelUtils.java
@@ -19,4 +19,18 @@ public class PixelUtils {
         }
         return destination;
     }
+
+    public static String[] toArrayOfTiles(Pixel[][] source){
+        int width = source.length-1;
+        int height = source[0].length-1;
+        String[] array = new String[(width+1)*(height+1)];
+        int array_position = 0;
+        for (int y = 0; y <= height; y++) {
+            for (int x = 0; x <= width; x++) {
+                    array[array_position] = source[x][y].getType().getTile();
+                    array_position++;
+                }
+            }
+        return  array;
+    }
 }
---
+++ rpg/src/test/java/dms/pastor/prototypes/aberminegenerator/model/PixelUtilsTest.java
@@ -0,0 +1,22 @@
+package dms.pastor.prototypes.aberminegenerator.model;
+
+import dms.pastor.prototypes.aberminegenerator.model.generators.WorldGenerator;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PixelUtilsTest {
+
+    @Test
+    void toArrayOfTilesAcceptanceTest() {
+        // given
+        final Pixel[][] worldAs2DArray = WorldGenerator.generateTestWorld().getWorld();
+        final String expectedResult = "########,,,,,##OOO,O##,,,,,##O,OOO##,,,,,##,,,,,########";
+
+        // when
+        final String result = String.join("",PixelUtils.toArrayOfTiles(worldAs2DArray));
+
+        // then
+        assertThat(result).isEqualTo(expectedResult);
+    }
+}
