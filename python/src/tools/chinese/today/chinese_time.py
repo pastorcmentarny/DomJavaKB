0ad772c877cf145bd3a8467955dbbeeff04078ef
PastorCmentarny <dmspastor@gmail.com>
2020-05-07 15:15:23 +0100

    fix display zero in time when you shouldn't

--- python/src/tools/chinese/today/chinese_time.py
+++ python/src/tools/chinese/today/chinese_time.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import datetime
+import random
 
 from tools.chinese.today import chinese_number
 
@@ -12,6 +13,8 @@ def get_current_time_in_chinese() -> str:
 
 def get_time_in_chinese_for(hour: int, minute: int) -> str:
     time_in_chinese = chinese_number.get_chinese_number(hour) + "点"
+    if minute == 0:
+        time_in_chinese += ''
     if minute == 15:
         time_in_chinese += '一刻'
     elif minute == 30:
@@ -22,3 +25,8 @@ def get_time_in_chinese_for(hour: int, minute: int) -> str:
         time_in_chinese += chinese_number.get_chinese_number(minute)
     return time_in_chinese
 
+
+def get_random_sleep_time():
+    typical_sleep_time = [get_time_in_chinese_for(22, 30), get_time_in_chinese_for(22, 45),
+                          get_time_in_chinese_for(23, 0), get_time_in_chinese_for(23, 15), '半夜']
+    return typical_sleep_time[random.randint(0, len(typical_sleep_time) - 1)]
