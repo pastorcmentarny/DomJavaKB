eeedbf4b982ab146c014808aa15f6e3f92269d66
PastorCmentarny <dmspastor@gmail.com>
2020-06-21 15:14:44 +0100

    add method to quirky utils to remember with enconding are supporting in json

---
+++ spring/src/main/java/dms/pastor/spring/commons/QuirkyUtils.java
@@ -0,0 +1,28 @@
+package dms.pastor.spring.commons;
+
+import com.fasterxml.jackson.core.JsonEncoding;
+
+import java.util.EnumSet;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * Author Dominik Symonowicz
+ * Created 17.06.2020
+ * WWW:	http://pastor.ovh.org
+ * Github:	https://github.com/pastorcmentarny
+ * Google Play:	https://play.google.com/store/apps/developer?id=Dominik+Symonowicz
+ * LinkedIn: uk.linkedin.com/pub/dominik-symonowicz/5a/706/981/
+ */
+public class QuirkyUtils {
+    private QuirkyUtils() {
+    }
+
+    //to remember what encoding are supported in Jackson
+    public static Map<String, JsonEncoding> getJsonEncodingsAsMap() {
+        return EnumSet.allOf(JsonEncoding.class).stream()
+                .collect(Collectors.toMap(JsonEncoding::getJavaName, Function.identity()));
+
+    }
+}
