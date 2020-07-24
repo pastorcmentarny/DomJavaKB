8330dded655426c0d5193ec1083667cbf6ce5248
pastor cmentarny <dmspastor@gmail.com>
2020-07-24 19:28:57 +0100

    It turns out that $ is a valid class and method name :)

---
+++ java/src/main/java/dms/pastor/examples/$.java
@@ -0,0 +1,12 @@
+package dms.pastor.examples;
+
+//LOL, this is a valid code in Java :D
+public class $ {
+    public static String $() {
+        return "$";
+    }
+
+    public static void main(String[] args) {
+        System.out.println($.$());
+    }
+}
