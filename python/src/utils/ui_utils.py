e1486166d8257dddd8955236b9199220bab4a30e
pastor cmentarny <dmspastor@gmail.com>
2020-07-25 11:15:47 +0100

    add display title function for lotto

---
+++ python/src/utils/ui_utils.py
@@ -0,0 +1,3 @@
+def title(text: str):
+    fix = '-' * 8
+    print(f'{fix} {text} {fix}')
---
+++ python/test/test_lotto_ui_utils.py
@@ -0,0 +1,23 @@
+import sys
+
+import io
+import unittest
+import unittest.mock
+
+from src.utils import ui_utils
+
+
+class MyTestCase(unittest.TestCase):
+
+    # tag-test-print
+    @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
+    def test_output(self, mock_stdout):
+        # given
+        text = 'test'
+        expected_result = '-------- test --------'
+
+        # when
+        ui_utils.title(text)
+
+        # then
+        self.assertEqual(expected_result, mock_stdout.getvalue().strip())
