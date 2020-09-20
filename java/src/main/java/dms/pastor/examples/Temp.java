e7f1871a6587bbf8716e7c286bebd53d3c691bfe
pastor cmentarny <dmspastor@gmail.com>
2020-09-20 11:32:19 +0100

    learning temp in java

---
+++ java/src/main/java/dms/pastor/examples/Temp.java
@@ -0,0 +1,50 @@
+package dms.pastor.examples;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class TemporaryFilesANdDirectoryExample {
+    private static String customPrefix = "data_";
+    private static String customFileSuffix = ".txt";
+    private static Path customBaseDirectory = FileSystems.getDefault().getPath("/Users/symonowd/Dropbox/banana");
+
+    public static void main(String[] args) throws IOException {
+        getCurrentTemporaryFilesPath();
+        createTemporaryFileWithPrefixAndSuffix();
+        createTemporaryStorageWithExitOnClose();
+    }
+
+    private static void createTemporaryStorageWithExitOnClose() throws IOException {
+        final Path tempDirectory = Files.createTempDirectory(customBaseDirectory, customPrefix);
+        System.out.println(tempDirectory);
+        Path fileOne = Files.createTempFile(customBaseDirectory, customPrefix, customFileSuffix);
+        System.out.println(fileOne);
+        Path fileTwo = Files.createTempFile(customBaseDirectory, customPrefix, customFileSuffix);
+        System.out.println(fileTwo);
+
+        try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(tempDirectory)) {
+
+            tempDirectory.toFile().deleteOnExit();
+            for (Path file : directoryStream) {
+                file.toFile().deleteOnExit();
+                System.out.println(file);
+            }
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    private static void createTemporaryFileWithPrefixAndSuffix() throws IOException {
+        Path tmpCustomPrefixAndSuffix = Files.createTempFile(customPrefix, customFileSuffix);
+        System.out.println(tmpCustomPrefixAndSuffix);
+    }
+
+    private static void getCurrentTemporaryFilesPath() {
+        System.out.println(System.getProperty("java.io.tmpdir"));
+    }
+}
