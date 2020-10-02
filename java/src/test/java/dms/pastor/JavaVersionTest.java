d03b13e588f9e2c530717d062f336a61fd29eed1
Dominik Symonowicz <dmspastor@gmail.com>
2020-10-02 09:33:32 +0100

    Add test to ensure I am running on the last version of Java

---
+++ java/src/test/java/dms/pastor/JavaVersionTest.java
@@ -0,0 +1,25 @@
+package dms.pastor;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class JavaVersionTest {
+
+
+    @Test //Update me, when ne
+    public void shouldRunTestOnSpecificVersionOfJava() {
+        // given
+        String latestJavaVersion = "15";
+
+        // when
+        final var result = System.getProperty("java.version");
+
+        // then
+        assertThat(result).isEqualTo(latestJavaVersion);
+
+        // debug
+        System.out.println(result);
+    }
+}
