6984911297fe1a486a250e528d948a16926040cb
pastor cmentarny <dmspastor@gmail.com>
2020-11-14 15:56:35 +0000

    add get_project_path() in config to get path for file depens on device i use

--- python/src/tools/lotto/config.py
+++ python/src/tools/lotto/config.py
@@ -1,3 +1,5 @@
+import platform
+
 path = {
     'base': '../../../../../data/lotto/'
 }
@@ -7,3 +9,11 @@ def display_warning_message(path):
     print(
         "\n\n WARNING! \n\n If you see all result are 0s then format all results in: \n\t " + path +
         ".\n\t\t I need improve this in the future.Apologize from Dominik in the past. \n\n")
+
+
+def get_project_path(file: str):
+    device = {
+        'DomL5': 'D:/Projects/DomJavaKB/data/lotto/',
+        'DomL6': 'B:/GitHub/DomJavaKB/data/lotto/',
+    }
+    return f'{device[platform.node()]}{file}'
