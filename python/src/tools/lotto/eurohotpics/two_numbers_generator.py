28b2b60ce19135fa43a3b189ce18b2dcebb67274
Dominik Symonowicz <dmspastor@gmail.com>
2020-11-21 00:10:54 +0000

    update 2 number generator

--- python/src/tools/lotto/eurohotpics/two_numbers_generator.py
+++ python/src/tools/lotto/eurohotpics/two_numbers_generator.py
@@ -1,9 +1,11 @@
 import logging
+import webbrowser
 
 from src.tools.lotto import config
-# SETTINGS
-from src.tools.lotto.utils import draws_downloader, lotto_utils, output, get_result_for
+from src.tools.lotto.utils import draws_downloader, lotto_utils, output
 
+# SETTINGS
+open_lotto_website_in_webrowser = False
 url = 'https://www.national-lottery.co.uk/results/euromillions-hotpicks/draw-history/csv'
 path = lotto_hotpics_history_path = config.get_project_path('euro-hotpics-draws.csv')
 all_draws = config.get_project_path('euro-hotpicks-all-draws.csv')
@@ -11,42 +13,40 @@ logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - 
                     filename=config.get_project_path('log.txt'))
 
 data = draws_downloader.get_draws_for(url, path)
+
+
 #  draws_downloader.update_all_draws(data, all_draws)
 
-numbers = {}
 
-for i in range(1, 51):
-    numbers[str(i)] = 0
+def generate_two_random_numbers():
+    numbers = {}
 
-for line in data[1: len(data)]:
-    for i in range(1, lotto_utils.get_last(5)):
-        numbers[line[i]] = numbers.get(line[i], 0) + 1
+    for i in range(1, 51):
+        numbers[str(i)] = 0
 
-numbers_to_delete = []
-for line in data[0:10]:
-    for i in range(1, lotto_utils.get_last(5)):
-        numbers_to_delete.append(line[i])
+    for line in data[1: len(data)]:
+        for i in range(1, lotto_utils.get_last(5)):
+            numbers[line[i]] = numbers.get(line[i], 0) + 1
 
-numbers_to_delete = list(set(numbers_to_delete))
-logging.debug(numbers_to_delete)
+    numbers_to_delete = []
+    for line in data[0:10]:
+        for i in range(1, lotto_utils.get_last(5)):
+            numbers_to_delete.append(line[i])
 
-for value in numbers_to_delete:
-    numbers.pop(value)
+    numbers_to_delete = list(set(numbers_to_delete))
+    logging.debug(numbers_to_delete)
 
-numbers = output.display_numbers(numbers)
+    for value in numbers_to_delete:
+        numbers.pop(value)
 
-remaining_numbers = {}
-least_value = int(numbers[-1][1])
-print(least_value)
+    numbers_to_select = numbers
+    output.display_numbers(numbers)
 
-while len(remaining_numbers) < 2:
-    for key, value in numbers:
-        if value == least_value:
-            remaining_numbers.update({key: value})
-    least_value += 1
+    return lotto_utils.select_random_number_from_two_highest_len_of_played_game(
+        lotto_utils.sort_with_key_as_number_game_played(numbers_to_select))
 
-get_result_for.get_result(data, remaining_numbers, 2, 5)
 
-print(remaining_numbers)
-
-# webbrowser.open('https://www.national-lottery.co.uk/games/euromillions-hotpicks')
+if __name__ == '__main__':
+    print(f'result: {generate_two_random_numbers()}')
+    if open_lotto_website_in_webrowser:
+        webbrowser.open('https://www.national-lottery.co.uk/games/euromillions-hotpicks')
