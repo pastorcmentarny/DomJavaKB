acd5f2f0c5653bcc0f22a3f95273e323b7505dfe
Dominik Symonowicz <dmspastor@gmail.com>
2020-12-04 22:52:02 +0000

    fix various issue with euromillions number generator

--- python/src/tools/lotto/euromillions/euromillions_random_draw.py
+++ python/src/tools/lotto/euromillions/euromillions_random_draw.py
@@ -1,24 +1,23 @@
 import csv
 import logging
 import random
-import sys
+from timeit import default_timer as timer
 
 from src.tools.lotto import config
-from src.tools.lotto.utils import draws_downloader
+from src.tools.lotto.utils import draws_downloader, output, lotto_utils
 
-sys.path.insert(0, '../utils')
-
-# SETTINGS
+# ====== SETTINGS ========
+open_lotto_website_in_webrowser = config.settings['open_page']
+debug_mode = config.settings['debug_mode']
 url = 'https://www.national-lottery.co.uk/results/euromillions/draw-history/csv'
 path = lotto_hotpics_history_path = config.get_project_path('euro-draws.csv')
-# all_draws = config.get_project_path('euro-all-draws.csv')
+all_draws = config.get_project_path('euro-all-draws.csv')
+# ====== -------- ========
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s',
                     filename=config.get_project_path('log.txt'))
 
 data = draws_downloader.get_draws_for(url, path)
-# draws_downloader.update_all_draws(data, all_draws)
-
-
+WRITABLE = 'w'
 numbers = []
 
 
@@ -62,26 +61,76 @@ def update_data():
     for draw in data:
         draw_line = [draw[9], draw[0], draw[1], draw[2], draw[3], draw[4], draw[5], draw[6], draw[7]]
         cleaned_data.append(draw_line)
-    last_column = len(data[0]) - 1
-    print(last_column)
+    if debug_mode:
+        print(f'first line: {data[0]}')
+        print(f'no. of column in first line : {len(data[0])}')
+
     all_draws_file = open(all_draws)
     all_draws_list = list(csv.reader(all_draws_file))
-
-    last_draw = int(all_draws_list[0][last_column])
+    last_column = len(all_draws_list[0]) - 1
+    print(last_column)
+    print(all_draws_list[0])
+    last_draw = int(all_draws_list[0][0])
     draw_to_add = []
-    current_draw = int(recent_draws_list[0][last_column])
+    current_draw = int(cleaned_data[0][0])
     counter = 0
-    while last_draw != current_draw:
-        draw_to_add.append(recent_draws_list[counter])
+    while last_draw != current_draw:  # FIXME
+        print(f'current draw:{current_draw}, last draw: {last_draw}')
+        draw_to_add.append(cleaned_data[counter])
         counter += 1
-        current_draw = int(recent_draws_list[counter][last_column])
+        current_draw = int(cleaned_data[counter][0])
 
     print(draw_to_add)
-    all_draws_file = open(all_draws_file_path, WRITABLE)
-    all_draws_file = update_file_for(all_draws_file, draw_to_add)
-    all_draws_file = update_file_for(all_draws_file, all_draws_list)
+    all_draws_file = open(all_draws, WRITABLE)
+
+    all_draws_file = draws_downloader.update_file_for(all_draws_file, draw_to_add)
+    all_draws_file = draws_downloader.update_file_for(all_draws_file, all_draws_list)
     all_draws_file.close()
 
+
+def stats():
+    all_draws_file = open(all_draws)
+    all_draws_list = list(csv.reader(all_draws_file))
+    stars = {}
+    stars_pair = []
+    for a_draw in all_draws_list:
+        first_number = int(a_draw[7].strip())
+        second_number = int(a_draw[8].strip())
+        stars[first_number] = stars.get(first_number, 0) + 1
+        stars[second_number] = stars.get(second_number, 0) + 1
+        stars_pair.append((first_number, second_number))
+    print(f'stars count: {stars}')
+    print(f'stars pairs count: {lotto_utils.count_pairs(stars_pair)}')
+    last_played()
+
+
+def last_played():
+    all_draws_file = open(all_draws)
+    all_draws_list = list(csv.reader(all_draws_file))
+    last_played_list = {}
+    last_played_stars_list = {}
+
+    for number in range(1, 51):
+        last_played_list[number] = 'Never'
+        if number <= 12:
+            last_played_stars_list[number] = 'Never'
+
+    for draw in all_draws_list:
+        print(draw)
+        for column in range(2, 7):
+            lotto_utils.assign_last_played_if_never_play_before(int(draw[column]), draw[1], last_played_list)
+        for column in range(7,9):
+            lotto_utils.assign_last_played_if_never_play_before(int(draw[column]), draw[1], last_played_stars_list)
+
+    print(last_played_list)
+    print(last_played_stars_list)
+
+
 if __name__ == '__main__':
-    #update_data()
+    start_time = timer()
+    update_data()
+    stats()
     draw()
+    output.debug_mode_warning()
+    end_time = timer()
+    print('It took {} ms.'.format(int((end_time - start_time) * 1000)))
