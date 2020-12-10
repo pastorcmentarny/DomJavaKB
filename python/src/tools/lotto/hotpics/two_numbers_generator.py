9a71714871084932b32c861634d00aeaeded24dd
Dominik Symonowicz <dmspastor@gmail.com>
2020-12-10 22:07:07 +0000

    clean up two_numbers_generator for hotpicks

--- python/src/tools/lotto/hotpics/two_numbers_generator.py
+++ python/src/tools/lotto/hotpics/two_numbers_generator.py
@@ -13,7 +13,7 @@ from src.tools.lotto.utils import draws_manager
 from src.tools.lotto.utils import lotto_utils
 from src.tools.lotto.utils import output
 
-data = draws_manager.get_recent_draws_for_lotto_and_hotpicks()
+recent_draws_data = draws_manager.get_recent_draws_for_lotto_and_hotpicks()
 
 
 def generate_two_random_numbers():
@@ -22,14 +22,14 @@ def generate_two_random_numbers():
     for number in range(1, 60):
         numbers[str(number)] = 0
 
-    for line in data[1: len(data)]:
+    for line in recent_draws_data[1: len(recent_draws_data)]:
         for number in range(1, lotto_utils.get_last(6)):
             numbers[line[number]] = numbers.get(line[number], 0) + 1
 
     output.display_numbers(numbers)
 
     numbers_to_delete = []
-    for line in data[0:10]:
+    for line in recent_draws_data[0:10]:
         for number in range(1, lotto_utils.get_last(6)):
             numbers_to_delete.append(line[number])
 
