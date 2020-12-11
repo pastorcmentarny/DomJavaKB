5df404ca5a88c27591822a5a0e7a5dccb21405d3
Dominik Symonowicz <dmspastor@gmail.com>
2020-12-11 11:09:24 +0000

    WIP euromillions analysis prototype

---
+++ python/src/tools/lotto/euromillions/euromillions_analysis.py
@@ -0,0 +1,155 @@
+"""
+1. Load recent draws data
+1. Load all draws data
+2. Process data
+3. Display data
+4. Save data
+
+# name, recent_count, all_count, last_played,
+"""
+import time
+
+from src.tools.lotto import config
+from src.tools.lotto.utils import draws_manager, output, lotto_utils
+
+recent_draws_data = draws_manager.get_recent_draws_for_euromillions()
+all_draw_data = draws_manager.load_all_draws_for_euromillions()
+games = int(all_draw_data[0][0])
+
+
+class Number:
+    def __init__(self, number: int, recent_games_counter: int, all_games_counter: int, last_played_date: str,
+                 last_played_counter, play_between_games: dict, recent_average_play: float,
+                 all_draws_average_play: float):
+        self._ball = number
+        self._recent_games_counter = recent_games_counter
+        self._all_games_counter = all_games_counter
+        self._last_played_date = last_played_date
+        self._last_played_counter = last_played_counter
+        self._play_between_games = play_between_games
+        self._recent_average_play = recent_average_play
+        self._all_draws_average_play = all_draws_average_play
+
+    def set_recent(self, recent_counter: int):
+        self._recent_games_counter = recent_counter
+
+    def set_all_draws(self, all_draws: int):
+        self._all_games_counter = all_draws
+
+    def set_last_played_date(self, last_played_date: str):
+        if self._last_played_date == 'Never':
+            self._last_played_date = last_played_date
+
+    def set_last_played_counter(self, last_played_counter: int):
+        if self._last_played_counter == -1:
+            self._last_played_counter = last_played_counter
+
+    def set_recent_average_play(self, recent_average_play: float):
+        if self._recent_average_play == -1:
+            self._recent_average_play = recent_average_play
+
+    def set_all_draws_average_play(self, all_draws_average_play: float):
+        if self._all_draws_average_play == -1:
+            self._all_draws_average_play = all_draws_average_play
+
+    def info(self) -> str:
+        return f'{self._ball} : {self._recent_games_counter} : {self._all_games_counter} : {self._last_played_date}' \
+               f' ({self._last_played_counter} draws ago) : {"%.2f" % self._recent_average_play} : {"%.2f" % self._all_draws_average_play}'
+
+    def __repr__(self):
+        return repr((self._ball, self._recent_games_counter, self._all_games_counter, self._last_played_date,
+                     self._last_played_counter, self._play_between_games, self._recent_average_play,
+                     self._all_draws_average_play))
+
+
+numbers_data = {}
+
+for number in range(1, 51):
+    numbers_data[number] = Number(number, -1, -1, 'Never', -1, {}, -1, -1)
+
+
+def recent_updater():
+    recent_numbers = {}
+    for a_number_idx in range(1, 51):
+        recent_numbers[int(a_number_idx)] = 0
+    print(f'recent_numbers len {len(recent_numbers)}')
+
+    for line in recent_draws_data[1: len(recent_draws_data)]:
+        for column_idx in range(2, lotto_utils.get_last(7)):
+            print('before :' +str(recent_numbers[int(line[column_idx])]))
+            recent_numbers[int(line[column_idx])] = recent_numbers.get(column_idx) + 1
+            print('after :' +str(recent_numbers[int(line[column_idx])]))
+    print(f'recent_numbers len {len(recent_numbers)}')
+    time.sleep(0.5)
+
+    for recent_number in recent_numbers:
+        numbers_data.get(int(recent_number)).set_recent(recent_numbers[recent_number])
+        numbers_data.get(int(recent_number)).set_recent_average_play(float(recent_numbers[recent_number] / 51) * 100)
+
+    if config.settings['detailed_mode']:
+        output.display_numbers(recent_numbers, 'balls (recent):')
+
+
+def all_updater():
+    all_numbers = {}
+    for a_number in range(1, 51):
+        a_number = int(a_number)
+        all_numbers[a_number] = 0
+
+    for line in all_draw_data[0: len(all_draw_data)]:
+        for all_number in range(2, lotto_utils.get_last(7)):
+            all_numbers[line[all_number]] = all_numbers.get(line[all_number], 0) + 1
+
+    for a_number in all_numbers:
+        idx = int(a_number)
+        numbers_data.get(idx).set_all_draws(all_numbers[a_number])
+        numbers_data.get(idx).set_all_draws_average_play(float(idx / games) * 100)
+
+    for pos, a_draw in enumerate(all_draw_data):
+        for column in range(2, 7):
+            idx = int(a_draw[column])
+            numbers_data.get(idx).set_last_played_date(a_draw[1])
+            numbers_data.get(idx).set_last_played_counter(pos)
+
+
+def stats():
+    recent_updater()
+    all_updater()
+
+    stars = {}
+    stars_pair = []
+    for a_draw in all_draw_data:
+        first_number = int(a_draw[7].strip())
+        second_number = int(a_draw[8].strip())
+        stars[first_number] = stars.get(first_number, 0) + 1
+        stars[second_number] = stars.get(second_number, 0) + 1
+        stars_pair.append((first_number, second_number))
+    output.display_numbers(stars, 'stars count:')
+    output.display_numbers(lotto_utils.count_pairs(stars_pair), 'stars pairs count:')
+    last_played()
+
+
+def last_played():
+    all_draws_list = draws_manager.load_all_draws_for_euromillions()
+    last_played_list = {}
+    last_played_stars_list = {}
+
+    for number in range(1, 51):
+        last_played_list[number] = 'Never'
+        if number <= 12:
+            last_played_stars_list[number] = 'Never'
+
+    for a_draw in all_draws_list:
+        for column in range(2, 7):
+            lotto_utils.assign_last_played_if_never_play_before(int(a_draw[column]), a_draw[1], last_played_list)
+        for column in range(7, 9):
+            lotto_utils.assign_last_played_if_never_play_before(int(a_draw[column]), a_draw[1], last_played_stars_list)
+
+    output.display_numbers(last_played_list, 'last played number date:')
+    output.display_numbers(last_played_stars_list, 'last played stars date:')
+
+
+if __name__ == '__main__':
+    stats()
+    for number in numbers_data:
+        print(numbers_data.get(number).info())
