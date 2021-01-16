dbe62ea02cb1d1455289427d3d0ae366876cecb6
Dominik Symonowicz <dmspastor@gmail.com>
2021-01-16 21:03:20 +0000

    add results stats for lotto , hotpicks, euro and euro hotpicks no need for set for life

--- python/src/tools/lotto/tools/results_stats.py
+++ python/src/tools/lotto/tools/results_stats.py
@@ -1,7 +1,5 @@
 import csv
 
-import time
-
 from src.tools.lotto.utils import output
 
 
@@ -18,30 +16,47 @@ def get_percent_for(amount: int, total: int) -> str:
     return "{:.1f}".format(amount / total * 100)
 
 
-def generate_results_for(game: str, path):
+def generate_results_for(game: str, path, won_amount_column: int, ticket_cost: float):
     results = load_results_for(path)
     wins_count = 0
     wins_amount = 0
     all_games = len(results)
     for result in results:
         line = result[0].split(';')
-        win_amount = int(line[7])
+        win_amount = int(line[won_amount_column])
         if win_amount > 0:
             wins_count += 1
             wins_amount += win_amount
     print(
         f'You play {all_games} times.You won {wins_count} of {all_games}.\n'
         f'You win rate is {get_percent_for(wins_count, all_games)}%\n'
-        f'You spent £{all_games}. You won £{wins_amount}. Your balance is £{wins_amount - all_games}.')
+        f'You spent £{all_games}. You won £{wins_amount}. Your balance is £{wins_amount - all_games * ticket_cost}.')
+    output.debug_print(f'Results stats for {game} completed.')
 
 
 def generate_results():
-    game = 'thunderball'
-    output.draw_title(game)
-    generate_results_for(game, r'B:\GitHub\DomJavaKB\data\lotto\thunderball-results-2021.csv')
+    game = 'Lotto'
+    output.draw_title(game, 1)
+    generate_results_for(game, r'B:\GitHub\DomJavaKB\data\lotto\lotto-results-2021.csv', 1, 2)
+
+    game = 'Lotto Hotpicks'
+    output.draw_title(game, 1)
+    generate_results_for(game, r'B:\GitHub\DomJavaKB\data\lotto\lotto-hotpicks-results-2021.csv', 1, 1)
+
+    game = 'Euro Hotpicks'
+    output.draw_title(game, 1)
+    generate_results_for(game, r'B:\GitHub\DomJavaKB\data\lotto\euro-hotpicks-results-2021.csv', 1, 1.5)
+
+    game = 'EuroMillions'
+    output.draw_title(game, 1)
+    generate_results_for(game, r'B:\GitHub\DomJavaKB\data\lotto\euro-results-2021.csv', 8, 2.5)
+
+    game = 'Thunderball'
+    output.draw_title(game, 1)
+    generate_results_for(game, r'B:\GitHub\DomJavaKB\data\lotto\thunderball-results-2021.csv', 7, 1)
 
 
 if __name__ == '__main__':
     print('Generating results...')
     generate_results()
-    print('Done.')
+    print('\nDone.')
