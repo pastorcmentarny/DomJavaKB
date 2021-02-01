ded11c958fe64b3d754081a044add3a77df55b07
Dominik Symonowicz <dmspastor@gmail.com>
2021-02-01 09:04:21 +0000

    change way to put input into euro hotpicks cleaner

--- python/src/tools/lotto/eurohotpics/eurohotpicks_result_cleaner.py
+++ python/src/tools/lotto/eurohotpics/eurohotpicks_result_cleaner.py
@@ -1,23 +1,32 @@
 import pyperclip
 
+NEW_LINE = "\n"
+EMPTY = ''
+
 
 def clean_result(results: str) -> str:
-    cleaned_result = ""
-    lines = results.split('\n')
+    cleaned_result = EMPTY
+    lines = results.split(NEW_LINE)
     temp_lines = []
     for line in lines:
-        if line != '':
+        if line != EMPTY:
             temp_lines.append(line.strip())
     lines = temp_lines.copy()
     for idx in range(0, len(lines), 2):
         line = lines[idx].strip()[2:] + lines[idx + 1].strip()
-        line = line.replace('Pick ', '').replace('-', '')
-        if line.strip() != '' and not line.replace(' ', '').isalpha():
+        line = line.replace('Pick ', EMPTY).replace('-', EMPTY)
+        if line.strip() != EMPTY and not line.replace(' ', EMPTY).isalpha():
             cleaned_result += f';0;{line.lstrip().replace(" ", ";")};\n'
-    return cleaned_result.rstrip("\n")
+    return cleaned_result.rstrip(NEW_LINE)
 
+
+results_input = """"""
 
 if __name__ == '__main__':
-    result = clean_result(pyperclip.paste())
-    print(result)
-    pyperclip.copy(result)
+    print('REMINDER! DO NOT FORGOT UPDATE INPUT BEFORE RUN IT')
+    if results_input == '':
+        print('^^^^^^^^ ^^^^^^^^ ^^^^^^^^ ^^^^^^^^ ^^^^^^^^')
+    else:
+        result = clean_result(results_input)
+        print(result)
+        pyperclip.copy(result)
