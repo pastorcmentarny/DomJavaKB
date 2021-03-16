50ce2f4c85648e479228db935c4cc0521badb1a6
Dominik Symonowicz <dmspastor@gmail.com>
2021-03-16 09:28:50 +0000

    replace my old non existing page to my current homepage https://dominiksymonowicz.com/

---
+++ spring/src/main/java/dms/pastor/spring/examples/cron/CronService.java
@@ -0,0 +1,9 @@
+package dms.pastor.spring.examples.cron;
+
+import org.springframework.stereotype.Service;
+/*
+DO THIS EXAMPLE: https://spring.io/blog/2020/11/10/new-in-spring-5-3-improved-cron-expressions
+ */
+@Service
+public class CronService {
+}
--- spring/src/test/java/dms/pastor/spring/vocabulizator/DefinitionBuilderTest.java
+++ spring/src/test/java/dms/pastor/spring/vocabulizator/DefinitionBuilderTest.java
@@ -15,7 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 /**
  * Author Dominik Symonowicz
  * Created 26/10/2016
- * WWW:	http://pastor.ovh.org
+ * WWW:	https://dominiksymonowicz.com/
  * Github:	https://github.com/pastorcmentarny
  * Google Play:	https://play.google.com/store/apps/developer?id=Dominik+Symonowicz
  * LinkedIn: uk.linkedin.com/pub/dominik-symonowicz/5a/706/981/
