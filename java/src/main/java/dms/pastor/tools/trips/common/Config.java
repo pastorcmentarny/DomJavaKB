8e491d7f76e327239b04fa89a635303d7778d1c5
Dominik Symonowicz <dmspastor@gmail.com>
2021-04-28 16:34:10 +0100

    move config to common package to be able to use for Tube Overground and Trains

---
+++ java/src/main/java/dms/pastor/tools/trips/common/Config.java
@@ -0,0 +1,5 @@
+package dms.pastor.tools.trips.common;
+
+public class Config {
+    public static final boolean SHOW_HIDDEN_OPTIONS = false;
+}
--- java/src/main/java/dms/pastor/tools/trips/tube/Config.java
+++
@@ -1,5 +0,0 @@
-package dms.pastor.tools.trips.tube;
-
-public class Config {
-    public static final boolean SHOW_HIDDEN_OPTIONS = true;
-}
