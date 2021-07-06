83fa297c33e9d66a6cb57c6ff9cff2dba77641b2
Dominik Symonowicz <dmspastor@gmail.com>
2021-07-06 09:15:07 +0100

    extract getDateAsStringOrNone method to Constants code clean up update lotto result

--- java/src/main/java/dms/pastor/tools/trips/common/Constants.java
+++ java/src/main/java/dms/pastor/tools/trips/common/Constants.java
@@ -2,12 +2,18 @@ package dms.pastor.tools.trips.common;
 
 import dms.pastor.tools.trips.common.station.StationType;
 
+import java.time.LocalDate;
+
 public class Constants {
     public static final int ALL_TUBE_STATIONS = 270;
     public static final int ALL_OVERGROUND_STATIONS = 112;
 
+    public static String getDateAsStringOrNone(LocalDate date) {
+        return date == null ? "none" : date.toString();
+    }
 
-    public static int get_all_stations_number_for(StationType type) {
+
+    public static int getAllStationsNumberFor(StationType type) {
 
         return switch (type) {
             case TUBE -> ALL_TUBE_STATIONS;
--- java/src/test/java/dms/pastor/tools/trips/common/ConstantsTest.java
+++ java/src/test/java/dms/pastor/tools/trips/common/ConstantsTest.java
@@ -3,26 +3,49 @@ package dms.pastor.tools.trips.common;
 import dms.pastor.tools.trips.common.station.StationType;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 class ConstantsTest {
 
     @Test
-    void get_all_stations_number_for_tube() {
+    void getAllStationsNumberForTube() {
         // when
-        int result = Constants.get_all_stations_number_for(StationType.TUBE);
+        int result = Constants.getAllStationsNumberFor(StationType.TUBE);
 
         // then
         assertThat(result).isEqualTo(Constants.ALL_TUBE_STATIONS);
     }
 
     @Test
-    void get_all_stations_number_for_overground() {
+    void getAllStationsNumberForOverground() {
         // when
-        int result = Constants.get_all_stations_number_for(StationType.OVERGROUND);
+        int result = Constants.getAllStationsNumberFor(StationType.OVERGROUND);
 
         // then
         assertThat(result).isEqualTo(Constants.ALL_OVERGROUND_STATIONS);
+    }
+
+    @Test
+    void getDateAsStringOrNoneReturnNoneForNull(){
+        // when
+        final var result = Constants.getDateAsStringOrNone(null);
+
+        // then
+        assertThat(result).isEqualTo("none");
+    }
+
+    @Test
+    void getDateAsStringOrNoneReturnNoneForToday(){
+        //given
+        final var todayDate = LocalDate.of(2021,4,8);
+
+        // when
+        final var result = Constants.getDateAsStringOrNone(todayDate);
+
+        // then
+        assertThat(result).isEqualTo("2021-04-08");
     }
 }
