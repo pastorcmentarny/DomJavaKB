f07ba184dc786036eaa28b8b7573a7471ffbe87f
Dominik Symonowicz <dmspastor@gmail.com>
2021-10-12 12:55:33 +0100

    add Transport Manager DB module as part of Transport Manager App that will manage all my transport related project update 40b440 update lotto

---
+++ TransportManagerDB/src/main/java/dom/coffirgar/transportmanager/TransportManagerDbApplication.java
@@ -0,0 +1,13 @@
+package dom.coffirgar.transportmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportManagerDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransportManagerDbApplication.class, args);
+    }
+
+}
---
+++ TransportManagerDB/src/test/java/dom/coffirgar/transportmanager/TransportManagerDbApplicationTests.java
@@ -0,0 +1,14 @@
+package dom.coffirgar.transportmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportManagerDbApplicationTests {
+
+    @Test
+    void applicationStarted() {
+
+    }
+
+}
