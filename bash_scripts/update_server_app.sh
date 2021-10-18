629611ef509515a73010aca2119509c45fec1854
Dominik Symonowicz <dmspastor@gmail.com>
2021-10-18 18:38:24 +0100

    move scripts into own directory

--- bash_scripts/update_server_app.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Updating Server App "
-rm server_app.py
-wget https://raw.githubusercontent.com/pastorcmentarny/DomJavaKB/master/python/serverstatus/server_app.py
-python3 server_app.py
-echo "Update complete"
---
+++ transportManager/scripts/ui.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+echo "Java"
+java --version
+echo "Starting app"
+java -jar /home/pi/TransportManagerService.jar
