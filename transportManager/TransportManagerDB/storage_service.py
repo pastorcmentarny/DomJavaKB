ba875ab7ded53ae8d339da41ddea7a2990256c5c
Dominik Symonowicz <dmspastor@gmail.com>
2021-10-19 10:39:19 +0100

    add store storage prototype

---
+++ transportManager/TransportManagerDB/storage_service.py
@@ -0,0 +1,41 @@
+SEPARATOR = ";;"
+EMPTY = ''
+
+READ = 'r'
+
+ENCODING = 'utf-8'
+
+
+def convert_line_to_dict(line: str) -> dict:
+    line_columns = line.split(SEPARATOR)
+    return {
+        "name": line_columns[0],
+        "status": line_columns[1],
+        "passedDate": line_columns[2],
+        "visitedDate": line_columns[3],
+        "thisYearVisitedDate;": line_columns[4],
+    }
+
+
+def convert_dict_to_line(station: dict) -> str:
+    return f'{station["name"]}{SEPARATOR}{station["status"]}{SEPARATOR}{station["passedDate"]}{SEPARATOR}{station["visitedDate"]}{SEPARATOR}{station["thisYearVisitedDate;"]}'
+
+
+def load_data(path: str) -> list:
+    file = open(path, READ, newline=EMPTY)
+    database = file.readlines()
+    database = [line.replace("\r\n", EMPTY) for line in database]
+    database = [convert_line_to_dict(line) for line in database]
+    return database
+
+
+def save_data(stations: list, path: str):
+    stations = [convert_line_to_dict(station) for station in stations]
+    with open(path, 'w+', encoding=ENCODING) as path_file:
+        path_file.write('\n'.join(stations))
+
+
+#prototype test
+if __name__ == '__main__':
+    x = convert_line_to_dict("St. James's Park;;V;;2017-02-02;;2018-06-01;;2021-10-03")
+    print(convert_dict_to_line(x))
