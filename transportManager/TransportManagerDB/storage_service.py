42374e70bb9a31139882d2f8dc709ed80745663c
Dominik Symonowicz <dmspastor@gmail.com>
2021-10-30 22:15:19 +0100

    code refactoring in storage service

--- transportManager/TransportManagerDB/storage_service.py
+++ transportManager/TransportManagerDB/storage_service.py
@@ -1,9 +1,15 @@
+VISITED_THIS_YEAR_DATE = "visitedThisYearDate"
+VISITED_DATE = "visitedDate"
+PASSED_DATE = "passedDate"
+STATUS = "status"
+NAME = "name"
+WRITE_READ_MODE = 'w+'
 SEPARATOR = ";;"
 EMPTY = ''
 
 READ = 'r'
 
-ENCODING = 'utf-8'
+ENCODING = 'UTF-8'
 
 
 # TODO replace with proper implementations
@@ -16,16 +22,16 @@ def convert_to_none_if_none_or_null(value: str):
 def convert_line_to_dict(line: str) -> dict:
     line_columns = line.split(SEPARATOR)
     return {
-        "name": line_columns[0],
-        "status": line_columns[1],
-        "passedDate": convert_to_none_if_none_or_null(line_columns[2]),
-        "visitedDate": convert_to_none_if_none_or_null(line_columns[3]),
-        "visitedThisYearDate": convert_to_none_if_none_or_null(line_columns[4]),
+        NAME: line_columns[0],
+        STATUS: line_columns[1],
+        PASSED_DATE: convert_to_none_if_none_or_null(line_columns[2]),
+        VISITED_DATE: convert_to_none_if_none_or_null(line_columns[3]),
+        VISITED_THIS_YEAR_DATE: convert_to_none_if_none_or_null(line_columns[4]),
     }
 
 
 def convert_dict_to_line(station: dict) -> str:
-    return f'{station["name"]}{SEPARATOR}{station["status"]}{SEPARATOR}{station["passedDate"]}{SEPARATOR}{station["visitedDate"]}{SEPARATOR}{station["visitedThisYearDate"]}'
+    return f'{station[NAME]}{SEPARATOR}{station[STATUS]}{SEPARATOR}{station[PASSED_DATE]}{SEPARATOR}{station[VISITED_DATE]}{SEPARATOR}{station[VISITED_THIS_YEAR_DATE]}'
 
 
 def load_data(path: str) -> list:
@@ -38,7 +44,5 @@ def load_data(path: str) -> list:
 
 def save_data(stations: list, path: str):
     stations = [convert_dict_to_line(station) for station in stations]
-    with open(path, 'w+', encoding=ENCODING) as path_file:
+    with open(path, WRITE_READ_MODE, encoding=ENCODING) as path_file:
         path_file.write('\n'.join(stations))
-
-
