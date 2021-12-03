bfdc9ceb6ebd8adc86a6bbd746c403dc270b6838
Dominik Symonowicz <dmspastor@gmail.com>
2021-12-03 22:28:41 +0000

    update menu page with all projects I am planning to do.

--- transportManager/TransportManagerUI/src/main/java/dms/coffirgar/transportmanager/configurations/Constants.java
+++ transportManager/TransportManagerUI/src/main/java/dms/coffirgar/transportmanager/configurations/Constants.java
@@ -3,6 +3,6 @@ package dms.coffirgar.transportmanager.configurations;
 public class Constants {
     private Constants(){}
     public static final String EMPTY = "";
-    public static final String TM_SERVICE_URL = "http://192.168.0.15:18002";
+    public static final String TM_SERVICE_URL = "http://192.168.0.3:18002"; //TODO REPLACE THAT CRAP
     //public static final String TM_SERVICE_URL = "http://192.168.0.205:18002";
 }
