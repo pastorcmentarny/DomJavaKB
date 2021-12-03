50065798c7e4524a0254f8bf628db981a9798be5
Dominik Symonowicz <dmspastor@gmail.com>
2021-12-03 22:53:37 +0000

    add logging and clean up code in server app

--- transportManager/DeviceStatusApp/server_app.py
+++ transportManager/DeviceStatusApp/server_app.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import psutil
 import re
 import requests
@@ -6,6 +7,9 @@ import time
 from blinkt import set_pixel, set_brightness, show, clear
 from subprocess import PIPE, Popen
 
+logger = logging.getLogger('app')
+# HOSTNAME = "http://192.168.0.205"
+HOSTNAME = "http://192.168.0.15"
 set_brightness(0.05)
 
 ALERT_COLOR = [255, 0, 0]
@@ -29,13 +33,20 @@ ALL_COLORS = [ALERT_COLOR, WARNING_COLOR, GOOD_COLOR, OFF_COLOR, UNKNOWN_COLOR, 
 7 DB
 """
 
+KEY_CPU = "CPU"
+KEY_SPACE = "SPACE"
+KEY_RAM = "KEY_RAM"
+KEY_TM_UI = "TM_UI"
+KEY_TM_SERVICE = "TM_SERVICE"
+KEY_TM_DB = "TM_DB"
+
 status = {
-    "CPU": UNKNOWN_COLOR,
-    "RAM": UNKNOWN_COLOR,
-    "SPACE": UNKNOWN_COLOR,
-    "TM_UI": UNKNOWN_COLOR,
-    "TM_SERVICE": UNKNOWN_COLOR,
-    "TM_DB": UNKNOWN_COLOR,
+    KEY_CPU: UNKNOWN_COLOR,
+    KEY_RAM: UNKNOWN_COLOR,
+    KEY_SPACE: UNKNOWN_COLOR,
+    KEY_TM_UI: UNKNOWN_COLOR,
+    KEY_TM_SERVICE: UNKNOWN_COLOR,
+    KEY_TM_DB: UNKNOWN_COLOR,
 }
 
 RED = 0
@@ -68,50 +79,50 @@ def get_space_available():
 
 
 def display_light():
-    set_pixel(0, status["CPU"][RED], status["CPU"][GREEN], status["CPU"][BLUE])
-    set_pixel(1, status["RAM"][RED], status["RAM"][GREEN], status["RAM"][BLUE])
-    set_pixel(2, status["SPACE"][RED], status["SPACE"][GREEN], status["SPACE"][BLUE])
+    set_pixel(0, status[KEY_CPU][RED], status[KEY_CPU][GREEN], status[KEY_CPU][BLUE])
+    set_pixel(1, status[KEY_RAM][RED], status[KEY_RAM][GREEN], status[KEY_RAM][BLUE])
+    set_pixel(2, status[KEY_SPACE][RED], status[KEY_SPACE][GREEN], status[KEY_SPACE][BLUE])
 
-    set_pixel(4, status["TM_UI"][RED], status["TM_UI"][GREEN], status["TM_UI"][BLUE])
-    set_pixel(5, status["TM_SERVICE"][RED], status["TM_SERVICE"][GREEN], status["TM_SERVICE"][BLUE])
-    set_pixel(6, status["TM_DB"][RED], status["TM_DB"][GREEN], status["TM_DB"][BLUE])
+    set_pixel(4, status[KEY_TM_UI][RED], status[KEY_TM_UI][GREEN], status[KEY_TM_UI][BLUE])
+    set_pixel(5, status[KEY_TM_SERVICE][RED], status[KEY_TM_SERVICE][GREEN], status[KEY_TM_SERVICE][BLUE])
+    set_pixel(6, status[KEY_TM_DB][RED], status[KEY_TM_DB][GREEN], status[KEY_TM_DB][BLUE])
     show()
 
 
 def cpu_health_check():
     temp = get_cpu_temperature()
     if temp > 70.0:
-        status["CPU"] = ALERT_COLOR
+        status[KEY_CPU] = ALERT_COLOR
     elif temp > 60.0:
-        status["CPU"] = WARNING_COLOR
+        status[KEY_CPU] = WARNING_COLOR
     elif temp > 55.0:
-        status["CPU"] = TEMP_ABOVE_55_COLOR
+        status[KEY_CPU] = TEMP_ABOVE_55_COLOR
     elif temp > 50.0:
-        status["CPU"] = TEMP_ABOVE_50_COLOR
+        status[KEY_CPU] = TEMP_ABOVE_50_COLOR
     elif temp > 45.0:
-        status["CPU"] = TEMP_ABOVE_40_COLOR
+        status[KEY_CPU] = TEMP_ABOVE_40_COLOR
     else:
-        status["CPU"] = GOOD_COLOR
+        status[KEY_CPU] = GOOD_COLOR
 
 
 def free_space_health_check():
     space = int(get_space_available())
     if space < 250:
-        status["SPACE"] = ALERT_COLOR
+        status[KEY_SPACE] = ALERT_COLOR
     elif space < 1000:
-        status["SPACE"] = WARNING_COLOR
+        status[KEY_SPACE] = WARNING_COLOR
     else:
-        status["SPACE"] = GOOD_COLOR
+        status[KEY_SPACE] = GOOD_COLOR
 
 
 def ram_available_health_check():
     ram = int(get_ram_available())
     if ram < 500:
-        status["RAM"] = ALERT_COLOR
+        status[KEY_RAM] = ALERT_COLOR
     elif ram < 250:
-        status["RAM"] = WARNING_COLOR
+        status[KEY_RAM] = WARNING_COLOR
     else:
-        status["RAM"] = GOOD_COLOR
+        status[KEY_RAM] = GOOD_COLOR
 
 
 def healthcheck():
@@ -121,11 +132,17 @@ def healthcheck():
     free_space_health_check()
 
 
+def log_status():
+    logger.info(
+        f"Status: CPU:{status[KEY_RAM]}, RAM:{status[KEY_CPU]}, SPACE:{status[KEY_SPACE]}, UI:{status[KEY_TM_UI]}, SERVICE:{status[KEY_TM_SERVICE]},DB:{status[KEY_TM_DB]}")
+
+
 def app_loop():
     while True:
         healthcheck()
         microservices_healthcheck()
         display_light()
+        log_status()
         time.sleep(5)
 
 
@@ -140,16 +157,17 @@ def get_data_for(url: str, timeout: int = 1) -> str:
             data_response = response.text
             return json.loads(data_response)["status"]
     except Exception as whoops:
+        logger.error(f"Unable to get data from url: {url} due to {whoops}")
         return "ERROR"
 
 
 def microservices_healthcheck():
-    ui = get_data_for("http://192.168.0.18:18001/actuator/health")
-    status["TM_UI"] = get_color_for_response(ui)
-    service = get_data_for("http://192.168.0.18:18002/actuator/health")
-    status["TM_SERVICE"] = get_color_for_response(service)
-    db = get_data_for("http://192.168.0.18:18003/actuator/health")
-    status["TM_DB"] = get_color_for_response(db)
+    ui = get_data_for(HOSTNAME + ":18001/actuator/health")
+    status[KEY_TM_UI] = get_color_for_response(ui)
+    service = get_data_for(HOSTNAME + ":18002/actuator/health")
+    status[KEY_TM_SERVICE] = get_color_for_response(service)
+    db = get_data_for(HOSTNAME + ":18003/actuator/health")
+    status[KEY_TM_DB] = get_color_for_response(db)
 
 
 def get_color_for_response(response):
@@ -186,6 +204,11 @@ def startup():
 
 
 if __name__ == '__main__':
+    logging_level = logging.DEBUG
+    logging_format = "%(levelname)s\t\t:: %(asctime)s :: %(message)s"
+    logging_filename = 'server.txt'
+    logging.basicConfig(level=logging_level, format=logging_format, filename='server.txt')
+    logger.info('Starting web server')
     startup()
     try:
         setup()
