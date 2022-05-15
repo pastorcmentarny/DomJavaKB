d62779437c1718f98b480549740916ad41b4837d
Dominik Symonowicz <dmspastor@gmail.com>
2022-05-15 14:19:45 +0100

    move transport manager project into standalone project update 40b440

--- transportManager/TransportManagerService/src/main/java/dom/coffirgar/transportmanager/configurations/Constants.java
+++
@@ -1,9 +0,0 @@
-package dom.coffirgar.transportmanager.configurations;
-
-public class Constants {
-    private Constants(){}
-
-    public static final String APPLICATION_JSON = "application/json";
-    public static final String TM_DB_URL = "http://192.168.0.3:18003";
-    //public static final String TM_DB_URL = "http://192.168.0.205:18003";
-}
